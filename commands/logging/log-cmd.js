6b881d9b90d03b218ada3cbdce1e09b82c4226b1
Donovan Daniels <dwddaniels262@gmail.com>
2018-11-10 16:58:33 -0600

    logging toggle command

--- commands/logging/log-cmd.js
+++ commands/logging/log-cmd.js
@@ -7,20 +7,46 @@ module.exports=(async (self,local) => {
 	if(["enable","en","e","disable","dis","d"].indexOf(self.args[0]) !== -1) {
 		var type = [...self.args];
 		type.shift();
-		var event = type.join("").toLowerCase();
-		if(!self.config.logging.types.includes(event)) return new Error("ERR_INVALID_USAGE");
-		
-		switch(args[0]) {
+		switch(self.args[0]) {
 			case "enable":
 			case "en":
 			case "e":
 				// enable
+				var a = type.join(" ").toLowerCase();
+				if(self.message.mentions.channels.first()) {
+					var a = a.replace(`<#${self.message.mentions.channels.first().id}>`,"");
+					var ch = self.message.mentions.channels.first();
+				}
+				var event = a.replace(" ","");
+				if(!self.config.logging.types.includes(event)) return new Error("ERR_INVALID_USAGE");
+
+				if(!self.gConfig.logging[event].enabled === true) {
+					if(!ch) ch = self.channel;
+					self.db.updateGuild(self.guild.id,{logging:{[event]:{channel:ch.id,enabled:true}}});
+					return self.message.reply(`Now logging **${event}** in <#${ch.id}>!`);
+				} else {
+					return self.message.reply(`The event **${event}** is already being logged in <#${self.gConfig.logging[event].channel}>.`);
+				}
 				break;
 				
 			case "disable":
 			case "dis":
 			case "d":
 				// disable
+				var a = type.join(" ").toLowerCase();
+				if(self.message.mentions.channels.first()) {
+					var a = a.replace(`<#${self.message.mentions.channels.first().id}>`,"");
+					var ch = self.message.mentions.channels.first();
+				}
+				var event = a.replace(" ","");
+				if(!self.config.logging.types.includes(event)) return new Error("ERR_INVALID_USAGE");
+
+				if(self.gConfig.logging[event].enabled === true) {
+					self.db.updateGuild(self.guild.id,{logging:{[event]:{channel:null,enabled:false}}});
+					return self.message.reply(`Stopped logging **${event}**!`);
+				} else {
+					return self.message.reply(`The event **${event}** is not currently being logged.`);
+				}
 				break;
 		}
 	} else {
