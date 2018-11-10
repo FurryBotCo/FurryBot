3a49e62d4d3d1663e87957fe3554de826ad4105e
Donovan Daniels <dwddaniels262@gmail.com>
2018-11-10 01:43:29 -0600

    fix args

--- commands/developer/reload-cmd.js
+++ commands/developer/reload-cmd.js
@@ -1,42 +1,40 @@
-module.exports=(async (message, gConfig) => {
-	if(!message) return new Error ("missing message parameter");
-	if(!gConfig) return new Error ("missing gConfig parameter");
-	await require(`../../BaseCommand.js`)(message, gConfig);
-	if (config.developers.indexOf(message.author.id) == -1) {
-		return message.reply("You cannot run this command as you are not a bot owner.");
+module.exports = (async (self,local) => {
+	Object.assign(self,local);
+	if (self.config.developers.indexOf(self.message.author.id) == -1) {
+		return self.message.reply("You cannot run this command as you are not a bot owner.");
 	}
 	var ty=new RegExp(/^(((command|cmd|c)?[s]?(module|m)?[s]?(list|l)?[s]?)||all)$/gi);
-	if(args.join("").length == 0) {
+	if(self.args.join("").length == 0) {
 		var type="cl";
 	} else {
-		if(!ty.test(args.join(""))) return message.reply("Invalid type");
-		var type=args.join("");
+		if(!ty.test(self.args.join(""))) return self.message.reply("Invalid type");
+		var type = self.args.join("");
 	}
 	switch(true) {
 		case new RegExp(/^(command|cmd|c)[s]?(list|l)?$/gi).test(type):
 			message.reply("Reloading command list..");
-			global.reloadCommands();
+			self.reloadCommands();
 			break;
 			
 		case new RegExp(/^((command|cmd|c)(module|m))[s]?$/gi).test(type):
-			message.reply("Reloading command/custom modules..");
-			global.reloadCommandModules();
+			self.message.reply("Reloading command/custom modules..");
+			self.reloadCommandModules();
 			break;
 			
 		case new RegExp(/^(module|m)[s]?$/gi).test(type):
-			message.reply("Reloading all modules");
-			global.reloadModules();
+			self.message.reply("Reloading all modules");
+			self.reloadModules();
 			break;
 			
 		case type == "all":
-			message.reply("Reloading everything..");
-			global.reloadAll();
+			self.message.reply("Reloading everything..");
+			self.reloadAll();
 			break;
 			
 		default:
-			return message.reply("Invalid reload type");
+			return self.message.reply("Invalid reload type");
 	}
 	setTimeout(()=>{
-		message.reply(`specified reloads finished ${config.emojis.furokaypaw}`);
+		self.message.reply(`specified reloads finished ${self.config.emojis.furokaypaw}`);
 	}, 1e3);
 });
