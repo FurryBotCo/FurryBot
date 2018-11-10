14391e4f7c848cef2db5c4740ebae5ccdd5fcd14
Donovan Daniels <dwddaniels262@gmail.com>
2018-11-10 01:44:12 -0600

    usage error &  fix inclusion

--- commands/fun/bap-cmd.js
+++ commands/fun/bap-cmd.js
@@ -1,13 +1,14 @@
-module.exports = (async (self) => {
+module.exports = (async (self,local) => {
+	Object.assign(self,local);
 	if(!self.args[0]) {
-		return new Error("INVALID_USAGE");
+		return new Error("ERR_INVALID_USAGE");
 	}
 	
 	var input = self.args.join(" ");
 	var text = self.varParse(self.c,{author:self.author,input:input});
 	if(self.gConfig.imageCommands) {
 		if(!self.channel.permissionsFor(self.guild.me).has("ATTACH_FILES")) return self.message.reply("Hey, I require the `ATTACH_FILES` permission for images to work on these commands!");
-		var attachment = new self.MessageAttachment("https://assets.furrybot.me/bap.gif");
+		var attachment = new self.Discord.MessageAttachment("https://assets.furrybot.me/bap.gif");
 		self.channel.send(text,attachment);
 	} else {
 		self.channel.send(text);
