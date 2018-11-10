2ec0b64a286b7a12aabab5eb669d4629a6281c5a
Donovan Daniels <dwddaniels262@gmail.com>
2018-11-10 01:47:23 -0600

    api error handling, fix inclusion

--- commands/fun/kiss-cmd.js
+++ commands/fun/kiss-cmd.js
@@ -1,14 +1,16 @@
-module.exports=(async (self) => {
+module.exports = (async (self,local) => {
+	Object.assign(self,local);
 	if(!self.args[0]) {
-		return new Error("INVALID_USAGE");
+		return new Error("ERR_INVALID_USAGE");
 	}
-
+	
 	var input = self.args.join(" ");
 	var text = self.varParse(self.c,{author:self.author,input:input});
-	if(self.gConfig.imageCommands) {
+	if(self.gConfig.commandImages) {
 		if(!self.channel.permissionsFor(self.guild.me).has("ATTACH_FILES")) return self.message.reply("Hey, I require the `ATTACH_FILES` permission for images to work on these commands!");
 		var img = await self.imageAPIRequest(true,self.command);
-		var attachment = new self.MessageAttachment(img.response.image);
+		if(!img.success) return self.message.reply(`Image API returned an error: ${img.error.description}`);
+		var attachment = new self.Discord.MessageAttachment(img.response.image);
 		self.channel.send(text,attachment);
 	} else {
 		self.channel.send(text);
