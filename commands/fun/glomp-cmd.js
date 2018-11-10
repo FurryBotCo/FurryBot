6ff00d28d5b9781ab6e5bd3cc0e83022e2af6095
Donovan Daniels <dwddaniels262@gmail.com>
2018-11-10 01:47:00 -0600

    fix inclusion & error handling

--- commands/fun/glomp-cmd.js
+++ commands/fun/glomp-cmd.js
@@ -1,6 +1,7 @@
-module.exports=(async (self) => {
+module.exports = (async (self,local) => {
+	Object.assign(self,local);
 	if(!self.args[0]) {
-		return new Error("INVALID_USAGE");
+		return new Error("ERR_INVALID_USAGE");
 	}
 	var text = self.varParse(self.c,{author:self.author,input:self.args.join(" ")});
 	self.channel.send(text);
