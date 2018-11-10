4fe25154a8097a430adde55c86e63773d90960c7
Donovan Daniels <dwddaniels262@gmail.com>
2018-11-10 01:42:14 -0600

    update sharder with new file name

--- sharder.js
+++ sharder.js
@@ -1,7 +1,7 @@
 const config = require("./config.js");
-const logger = require("./utility/logger.js");
+const logger = require("./util/logger.js");
 const { ShardingManager } = require('discord.js');
-const custom = Object.assign({}, require("./utility/misc.js"), require("./utility/functions.js"));
+const custom = Object.assign({}, require("./util/misc"), require("./util/functions"));
 //const manager = new ShardingManager(`bot.js`, {totalShards: 2});
 
 //manager.spawn();
@@ -9,7 +9,7 @@ const custom = Object.assign({}, require("./utility/misc.js"), require("./utilit
 if(config.beta) {
 	console.warn(`[ShardingManager] Warning! Launching beta version of bot.. (waiting ${.5e3/1000} seconds)`);
 	setTimeout(function(token){
-		const manager = new ShardingManager(`bot.js`, {token: config.bot.token, respawn: true, totalShards: config.bot.shardCount});
+		const manager = new ShardingManager(`index.js`, {token: config.bot.token, respawn: true, totalShards: "auto"});
 
 		manager.spawn();
 		manager.on('launch', (shard) => {
@@ -19,7 +19,7 @@ if(config.beta) {
 	}, .5e3, config.bot.token);
 } else {
 	console.log(`Launching normal bot..`);
-	const manager = new ShardingManager(`bot.js`, {token: config.bot.token, respawn: true, totalShards: config.bot.shardCount});
+	const manager = new ShardingManager(`index.js`, {token: config.bot.token, respawn: true, totalShards: "auto"});
 
 	manager.spawn();
 	manager.on('launch', (shard) => {
