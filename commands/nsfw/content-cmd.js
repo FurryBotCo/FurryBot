ab1dda366257249b23fb1f4e7f691335d45e33d9
Donovan Daniels <dwddaniels262@gmail.com>
2018-11-11 03:00:17 -0600

    fix content command & disable off-server

---
+++ commands/nsfw/content-cmd.js
@@ -0,0 +1,9 @@
+module.exports = (async (self,local) => {
+    Object.assign(self,local);
+    if(self.os.hostname() !== "FURRYBOT-SERVER") return self.message.reply("This bot is not running on its main server, so this command is disabled!");
+    const gay = await self.fsn.readdir("D:\\xampp\\websites\\furcdn.net\\webroot\\furrybot\\nsfw\\yiff\\gay").then((f)=>f.length),
+        straight = await self.fsn.readdir("D:\\xampp\\websites\\furcdn.net\\webroot\\furrybot\\nsfw\\yiff\\straight").then((f)=>f.length),
+        bulge = await self.fsn.readdir("D:\\xampp\\websites\\furcdn.net\\webroot\\furrybot\\nsfw\\bulge").then((f)=>f.length);
+
+        return self.channel.send(`Content Counts\n\n**Gay**: ${gay}\n**Straight**: ${straight}\n**Bulge**: ${bulge}\n**Total**: ${gay.length+straight.length+bulge.length}`);
+});
