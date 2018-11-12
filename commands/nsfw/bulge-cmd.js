015f5e8d153a90f06b6c1e64eb3facc33c153dbd
Donovan_DMC <dwddaniels262@gmail.com>
2018-11-12 11:21:22 -0600

    feat: ren shorturl imageNumber key to number

--- commands/nsfw/bulge-cmd.js
+++ commands/nsfw/bulge-cmd.js
@@ -6,6 +6,6 @@ module.exports = (async (self,local) => {
     }
     var attachment = new self.Discord.MessageAttachment(img.response.image);
     var short = await self.shortenUrl(img.response.image);
-    var extra = short.new ? `**This is the first time this has been viewed! Image #${short.imageNumber}**\n\n` : "";
+    var extra = short.new ? `**This is the first time this has been viewed! Image #${short.number}**\n\n` : "";
     return self.channel.send(`${extra}Short URL: <${short.link}>\n\nRequested By: ${self.author.tag}`,attachment);
 });
