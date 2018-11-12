27b00a9625adbdf042bfac5a189459fca4a8cdff
Donovan_DMC <dwddaniels262@gmail.com>
2018-11-12 17:12:07 -0600

    fix: proper sharding with counts

--- index.js
+++ index.js
@@ -8,22 +8,21 @@ const custom = Object.assign({}, require("./util/misc"), require("./util/functio
 //manager.on('launch', shard => {console.debug(`Successfully launched shard ${shard.id+1} (ID: ${shard.id})`);console.debug(`Memory: ${custom.getUsedMemory("MB")}/${custom.getTotalMemory("MB")}`)});
 if(config.beta) {
 	console.warn(`[ShardingManager] Warning! Launching beta version of bot.. (waiting ${.5e3/1000} seconds)`);
-	setTimeout(function(token){
-		const manager = new ShardingManager(`main.js`, {token: config.bot.token, respawn: true, totalShards: "auto"});
-
-		manager.spawn();
-		manager.on('launch', (shard) => {
-			console.debug(`Successfully launched shard ${shard.id+1} (ID: ${shard.id})`);
+	setTimeout(()=>{
+		const manager = new ShardingManager(`main.js`, {token: config.bot.token, respawn: true, totalShards: config.bot.shardCount, mode: "process"});
+		manager.on('shardCreate', (shard) => {
+			console.debug(`Successfully launched shard #${shard.id} (${+shard.id+1}/${shard.manager.totalShards}})`);
 			console.debug(`Memory: ${custom.getUsedMemory("MB")}/${custom.getTotalMemory("MB")}`)
 		});
-	}, .5e3, config.bot.token);
+		manager.spawn();
+	}, .5e3);
 } else {
 	console.log(`Launching normal bot..`);
-	const manager = new ShardingManager(`main.js`, {token: config.bot.token, respawn: true, totalShards: "auto"});
+	const manager = new ShardingManager(`main.js`, {token: config.bot.token, respawn: true, totalShards: config.bot.shardCount, mode: "process"});
 
-	manager.spawn();
-	manager.on('launch', (shard) => {
-		console.debug(`Successfully launched shard ${shard.id+1} (ID: ${shard.id})`);
+	manager.on('shardCreate', (shard) => {
+		console.debug(`Successfully launched shard #${shard.id} (${+shard.id+1}/${shard.manager.totalShards}})`);
 		console.debug(`Memory: ${custom.getUsedMemory("MB")}/${custom.getTotalMemory("MB")}`)
 	});
+	manager.spawn();
 }
