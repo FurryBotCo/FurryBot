1e4d2baf65adae00016db38ae2a9754c9e0c84fb
Donovan Daniels <dwddaniels262@gmail.com>
2018-11-14 01:49:03 -0600

    fix this tomorrow

--- util/dbFunctions.js
+++ util/dbFunctions.js
@@ -3,14 +3,14 @@ module.exports = ((self)=>{
 	return {
 		createGuild: (async(gid)=>{
 			if(typeof self === "undefined") {
-				self.log(`[dbFunctions.js:createGuild] Could not validate guild existnce`);
+				console.log(`[dbFunctions.js:createGuild] Could not validate guild existnce`);
 				return new Error("ERR_CANNOT_VALIDATE");
 			}
 			if(!self.guilds.has(gid)) {
-				self.log(`[dbFunctions.js:createGuild] Attempted to add guild to database that was not in bot, ${gid}`);
+				console.log(`[dbFunctions.js:createGuild] Attempted to add guild to database that was not in bot, ${gid}`);
 				return new Error("ERR_NOT_FOUND");
 			}
-			self.log(`Added guild ${gid} with default configuration.`);
+			console.log(`Added guild ${gid} with default configuration.`);
 			var gConf = {id: gid};
 			Object.assign(gConf, self.config.guildDefaultConfig);
 			await self.r.table("guilds").insert(gConf);
@@ -19,10 +19,10 @@ module.exports = ((self)=>{
 		deleteGuild: (async(gid)=>{
 			var guild = await self.r.table("guilds").get(gid);
 			if(!guild) {
-				self.log(`[dbFunctions.js:deleteGuild] Attempted to delete aguild from the database that was not present, ${gid}`);
+				console.log(`[dbFunctions.js:deleteGuild] Attempted to delete aguild from the database that was not present, ${gid}`);
 				return new Error("ERR_NOT_FOUND");
 			}
-			self.log(`Deleted guild ${gid}`);
+			console.log(`Deleted guild ${gid}`);
 			return self.r.table("guilds").get(gid).delete();
 		}),
 		getGuild: (async(gid)=>{
@@ -36,33 +36,33 @@ module.exports = ((self)=>{
 		}),
 		addWarning: (async(gid,uid,reason)=>{
 			if(typeof self === "undefined") {
-				self.log(`[dbFunctions.js:createGuild] Could not validate guild existence`);
+				console.log(`[dbFunctions.js:createGuild] Could not validate guild existence`);
 				return new Error("ERR_CANNOT_VALIDATE");
 			}
 			if(!self.guilds.has(guid)) {
-				self.log(`[dbFunctions.js:addWarning] Attempted to add warning to database for a guild that the bot is not in, ${gid}`);
+				console.log(`[dbFunctions.js:addWarning] Attempted to add warning to database for a guild that the bot is not in, ${gid}`);
 				return new Error("GUILD_NOT_FOUND");
 			}
 			return self.r.table("warnings").insert({gid,uid,reason});
 		}),
 		deleteWarning: (async(gid,wid)=>{
 			if(typeof self === "undefined") {
-				self.log(`[dbFunctions.js:createGuild] Could not validate guild existence`);
+				console.log(`[dbFunctions.js:createGuild] Could not validate guild existence`);
 				return new Error("ERR_CANNOT_VALIDATE");
 			}
 			if(!self.guilds.has(gid)) {
-				self.log(`[dbFunctions.js:addWarning] Attempted to delete a warning for a guild that the bot is not in, ${gid}`);
+				console.log(`[dbFunctions.js:addWarning] Attempted to delete a warning for a guild that the bot is not in, ${gid}`);
 				return new Error("ERR_NOT_FOUND");
 			}
 			var warn = await self.r.table("warnings").get(wid);
 
 			if(!warn) {
-				self.log(`[dbFunctions.js:deleteWarning] Attempted to delete a warning that does not exist.`);
+				console.log(`[dbFunctions.js:deleteWarning] Attempted to delete a warning that does not exist.`);
 				return new Error("ERR_NOT_FOUND");
 			}
 
 			if(warn.guildid !== gid) {
-				self.log(`[dbFunctions.js:deleteWarning] Attempted to delete a warning for a different guild. ${gid}/${warn.guildid}`);
+				console.log(`[dbFunctions.js:deleteWarning] Attempted to delete a warning for a different guild. ${gid}/${warn.guildid}`);
 				return new Error("ERR_UNAUTHORIZED");
 			}
 
@@ -70,11 +70,11 @@ module.exports = ((self)=>{
 		}),
 		purgeUserWarnings: (async(gid,uid)=>{
 			if(typeof self === "undefined") {
-				self.log(`[dbFunctions.js:createGuild] Could not validate guild existence`);
+				console.log(`[dbFunctions.js:createGuild] Could not validate guild existence`);
 				return new Error("ERR_CANNOT_VALIDATE");
 			}
 			if(!self.guilds.has(gid)) {
-				self.log(`[dbFunctions.js:addWarning] Attempted to delete a warning for a guild that the bot is not in, ${gid}`);
+				console.log(`[dbFunctions.js:addWarning] Attempted to delete a warning for a guild that the bot is not in, ${gid}`);
 				return new Error("ERR_NOT_FOUND");
 			}
 
@@ -97,7 +97,7 @@ module.exports = ((self)=>{
 			var donor = self.r.table("donors").get(uid);
 
 			if(!donor) {
-				self.log(`[dbFunctions.js:removeDonor] Attempted to delete a donor does not exist.`);
+				console.log(`[dbFunctions.js:removeDonor] Attempted to delete a donor does not exist.`);
 				return new Error("ERR_NOT_FOUND");
 			}
 
@@ -164,30 +164,70 @@ module.exports = ((self)=>{
 			}
 			return await self.r.table("stats").get("fCount").update({count: rs});
 		}),
-		createUser: (async(uid)=>{
-			self.log(`Created user ${uid} with default settings.`);
+		createUser: (async(uid,gid=null)=>{
+			console.log(`Created user ${uid} with default settings.`);
 			var uConf = {id: uid};
 			Object.assign(uConf, self.config.userDefaultConfig);
 			await self.r.table("users").insert(uConf);
+			await self.r.table("economy").get(gid);
 			return self.db.getUser(uid);
 		}),
-		deleteUser: (async(uid)=>{
+		deleteUser: (async(uid,gid=null)=>{
+			var obj = {};
 			var user = await self.r.table("users").get(uid);
 			if(!user) {
-				self.log(`[dbFunctions.js:deleteUser] Attempted to delete a user from the database that was not present, ${uid}`);
+				console.log(`[dbFunctions.js:deleteUser] Attempted to delete a user from the database that was not present, ${uid}`);
 				return new Error("ERR_NOT_FOUND");
 			}
-			self.log(`Deleted user ${uid}`);
-			return self.r.table("users").get(uid).delete();
+			obj.user = await self.r.table("users").get(uid).delete()
+			if(gid !== null) {
+				var economy = await self.r.table("economy").get(gid);
+				if(economy !== null) {
+					obj.economy = await self.r.table("economy").get(gid).replace(r.row.without({users:uid})).delete();
+				}
+			}
+			console.log(`[dbFunctions.js:deleteUser] Deleted user ${uid}`);
+			return obj;
 		}),
-		getUser: (async(uid)=>{
-			var user = await self.r.table("users").get(uid);
-			if(!user) return self.db.createUser(uid);
-			
-			return user;
+		getUser: (async(uid,gid=null)=>{
+			var obj = {};
+			var user = await self.r.table("users").get(uid)
+			Object.assign(obj,user);
+			if(!user) obj.user = await self.db.createUser(uid);
+			if(gid !== null) {
+				var e = await self.r.table("economy").get(gid);
+				var economy = e.users[uid];
+				if(economy !== null) Object.assign(obj,economy);
+			}
+			return obj;
 		}),
-		updateUser: (async(uid, fields)=>{
-			return self.r.table("users").get(uid).update(fields);
+		updateUser: (async(uid=null, gid=null, fields, type="users",ext)=>{
+			//return self.r.table("users").get(uid).update(fields);
+			var obj = {};
+			if(!ext.uid) return false;
+			if(!ext.fields) return false;
+			if(!ext.type) {
+				if(typeof ext.fields.users !== "undefined") {
+					obj.users = await self.r.table("users").get(ext.uid).update(ext.fields.users);
+				}
+				if(typeof ext.fields.economy !== "undefined") {
+					obj.economy = await self.r.table("economy").get(ext.gid).update(ext.fields.economy);
+				}
+			} else {
+				var type = !ext.type ? "furrybot" : ext.type; 
+				if(!["users","economy"].includes(type)) type = "furrybot";
+				switch(type) {
+					case "users":
+						var obj = await self.r.table(type).get(ext.uid).update(fields);
+						break;
+
+						case "economy":
+						var obj = await self.r.table(type).get(ext.gid).update(fields);
+						break;
+				}
+			}
+
+			return obj;
 		})
 	};
 });
