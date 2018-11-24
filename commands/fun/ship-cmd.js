c907ccb4272105bd297d61ecb45b17ca7750b24c
Donovan Daniels <dwddaniels262@gmail.com>
2018-11-24 16:20:36 -0600

    feat: ship command

--- commands/fun/ship-cmd.js
+++ commands/fun/ship-cmd.js
@@ -1,6 +1,6 @@
 module.exports = (async(self,local)=>{ 
     if(local.args.length < 1) return new Error("ERR_INVALID_USAGE");
-
+    local.channel.startTyping();
     if(local.args[0] === "random") {
         var user1 = local.guild.members.filter(u=>u.id!==local.author.id&&!u.user.bot).random();
     } else {
@@ -65,7 +65,8 @@ module.exports = (async(self,local)=>{
 		}
 		Object.assign(data, local.embed_defaults());
 		var embed = new self.Discord.MessageEmbed(data);
-		return local.channel.send(embed);
+        local.channel.send(embed);
+        return local.channel.stopTyping();
     }
 
     if(!user2) var user2 = local.member;
@@ -84,8 +85,37 @@ module.exports = (async(self,local)=>{
     var shipname = user1.username.substr(0,r1)+user2.username.substr(user2.username.length-r2,r2);
     var amount = Math.floor(Math.random()*101);
 
-    var heart = [undefined,null,""].includes(amount) ? "unknown" : amount <= 1 ? "1" : amount >= 2 && amount < 19 ? "2-19" : amount >= 20 && amount < 39 ? "20-39" : amount >= 40 && amount < 59 ? "40-59" : amount >= 60 && amount < 79 ? "60-79" : amount >= 80 && amount < 99 ? "80-99" : amount === 100 ? "100" : "unknown";
-
-    return local.message.reply(`I ship you as ${shipname}!~`);
-
+    const heart = [undefined,null,""].includes(amount) ? "unknown" : amount <= 1 ? "1" : amount >= 2 && amount < 19 ? "2-19" : amount >= 20 && amount < 39 ? "20-39" : amount >= 40 && amount < 59 ? "40-59" : amount >= 60 && amount < 79 ? "60-79" : amount >= 80 && amount < 99 ? "80-99" : amount === 100 ? "100" : "unknown";
+    const shiptext = [undefined,null,""].includes(amount) ? "unknown" : amount <= 1 ? "Not Happening.." : amount >= 2 && amount < 19 ? "Unlikely.." : amount >= 20 && amount < 39 ? "Maybe?" : amount >= 40 && amount < 59 ? "Hopeful!" : amount >= 60 && amount < 79 ? "Good!" : amount >= 80 && amount < 99 ? "Amazing!" : amount === 100 ? "Epic!" : "unknown";
+    const heartIcon = await self.fsn.readFile(`${self.config.rootDir}/assets/images/ship/ship-${heart}-percent.png`);
+    var u1 = user1.displayAvatarURL().split(".");
+	u1.pop();
+	var imgpath1 = `${self.config.rootDir}/tmp/${local.guild.id}-${local.channel.id}-${local.author.id}-ship-u1.png`;
+	await self.download(`${u1.join(".")}.png`,imgpath1);
+    var profile1 = await self.fsn.readFile(imgpath1);
+    var u2 = user2.displayAvatarURL().split(".");
+	u2.pop();
+	var imgpath2 = `${self.config.rootDir}/tmp/${local.guild.id}-${local.channel.id}-${local.author.id}-ship-u2.png`;
+	await self.download(`${u2.join(".")}.png`,imgpath2);
+	var profile2 = await self.fsn.readFile(imgpath2);
+    const img = new self.Canvas(384,128)
+    .addImage(profile1,0,0,128,128)
+    .addImage(heartIcon,128,0,128,128)
+    .addImage(profile2,256,0,128,128);
+    const shiph = await img.toBufferAsync();
+    var attch = new self.Discord.MessageAttachment(shiph,"ship.png");
+    var data = {
+        title: ":heart: **Shipping!** :heart:",
+        description: `Shipping **${user1.tag}** with **${user2.tag}**\n**${amount}%** - ${shiptext}\nShipname: ${shipname}`,
+        image: {
+            url: "attachment://ship.png"
+        }
+    }
+    Object.assign(data,local.embed_defaults());
+    var embed = new self.Discord.MessageEmbed(data);
+    embed.attachFiles(attch);
+    await local.channel.send(embed);
+    await self.fsn.unlink(imgpath1);
+    await self.fsn.unlink(imgpath2);
+    return local.channel.stopTyping();
 })
