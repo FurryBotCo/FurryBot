c412a43fa65c25e30f5892a82464852a053d2507
Donovan_DMC <dwddaniels262@gmail.com>
2018-12-01 10:54:32 -0600

    more events

--- handlers/events/channelCreate.js
+++ handlers/events/channelCreate.js
@@ -5,7 +5,39 @@ module.exports = (async(self,channel)=>{
     if(!gConfig || [undefined,null,"",{},[]].includes(gConfig.logging) || [undefined,null,"",{},[]].includes(gConfig.logging[ev]) || !gConfig.logging[ev].enabled || [undefined,null,""].includes(gConfig.logging[ev].channel)) return;
     var logch = channel.guild.channels.get(gConfig.logging[ev].channel);
     if(!logch) return self.db.updateGuild({logging:{[ev]:{enabled:false,channel:null}}});
+    switch(channel.type) {
+        case "text":
+            var typeText = ":pencil: Text";
+            break;
+
+        case "voice":
+            var typeText = ":loudspeaker: Voice";
+            break;
+
+        case "category":
+            var TypeText = "Category";
+            break;
+    }
     var data = {
-        title: `:new: ${typeText} Channel Created`
+        title: `:new: ${typeText} Channel Created`,
+        author: {
+            name: channel.guild.name,
+            icon_url: channel.guild.iconURL()
+        },
+        fields: [
+            {
+                name: "Name",
+                value: channel.name,
+                inline: true
+            },{
+                name: "ID"
+            }
+        ]
+    }
+
+    switch(channel.type) {
+        case "text":
+
+            break;
     }
 })
