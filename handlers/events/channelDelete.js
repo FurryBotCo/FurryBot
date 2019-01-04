c91f29bd0383a4ac6331366f039105a334dac609
Donovan_DMC <dwddaniels262@gmail.com>
2019-01-04 17:44:23 -0600

    channel events

--- handlers/events/channelDelete.js
+++ handlers/events/channelDelete.js
@@ -5,23 +5,9 @@ module.exports = (async(client,channel)=>{
     if(!gConfig || [undefined,null,"",{},[]].includes(gConfig.logging) || [undefined,null,"",{},[]].includes(gConfig.logging[ev]) || !gConfig.logging[ev].enabled || [undefined,null,""].includes(gConfig.logging[ev].channel)) return;
     var logch = channel.guild.channels.get(gConfig.logging[ev].channel);
     if(!logch) return client.db.updateGuild({logging:{[ev]:{enabled:false,channel:null}}});
-    switch(channel.type) {
-        case "text":
-            var typeText = ":pencil: Text";
-            break;
-
-        case "voice":
-            var typeText = ":loudspeaker: Voice";
-            break;
-
-        case "category":
-            var TypeText = "Category";
-            break;
-    }
-    var log = (await channel.guild.fetchAuditLogs({limit:1,type:"CHANNEL_DELETE"})).entries.first();
-    
+    if(!channel.deleted) return;
     var data = {
-        title: `:wastebasket: ${typeText} Channel Deleted`,
+        title: `:wastebasket: ${client.ucwords(channel.type)} Channel Deleted`,
         author: {
             name: channel.guild.name,
             icon_url: channel.guild.iconURL()
@@ -33,49 +19,63 @@ module.exports = (async(client,channel)=>{
 		},
         fields: [
             {
-                name: "Name",
-                value: channel.name,
+                name: "Channel",
+                value: `${channel.name} (${channel.id})`,
                 inline: false
             },{
-                name: "ID",
-                value: channel.id,
+                name: "Parent Channel",
+                value: [undefined,null,""].includes(channel.parent) ? "None" : `${channel.parent.name} (${channel.parent.id})`,
                 inline: false
             }
         ]
     }
 
-    if(typeof channel.parent !== "undefined") {
-        data.fields.push({
-            name: "Parent Channel Name",
-            value: channel.parent.name,
-            inline: false
-        },{
-            name: "Parent Channel ID",
-            value: channel.parent.id,
-            inline: false
-        });
+    switch(channel.type) {
+        case "text":
+            // topic, slowmode, & nsfw (text only)
+            data.fields.push({
+                name: "Topic",
+                value: [undefined,null,""].includes(channel.topic) ? "None" : channel.topic,
+                inline: false
+            },{
+                name: "Slowmode",
+                value: channel.rateLimitPerUser === 0 ? "None" : `${channel.rateLimitPerUser} Seconds`,
+                inline: false
+            },{
+                name: "NSFW",
+                value: channel.nsfw ? "Yes" : "No",
+                inline: false
+            })
+            break;
+
+        case "voice":
+            // bitrate & user limit (voice only)
+            data.fields.push({
+                name: "Bitrate",
+                value: `${channel.bitrate/1000}kbps`,
+                inline: false
+            },{
+                name: "Channel User Limit",
+                value: channel.userLimit === "0" ? "UNLIMITED" : channel.userLimit,
+                inline: false
+            });
+            break;
     }
-    if(channel.type === "voice") {
-        data.fields.push({
-            name: "Bitrate",
-            value: `${channel.bitrate/1000}kbps`,
+
+    // audit log check
+    if(channel.guild.me.permissions.has("VIEW_AUDIT_LOG")) {
+        var log = (await channel.guild.fetchAuditLogs({limit:1,type:"CHANNEL_DELETE"})).entries.first();
+        if(![undefined,null,"",[],{}].includes(log) && log.action === "CHANNEL_DELETE") {
+            data.fields.push({
+            name: "Executor",
+            value: log.executor instanceof client.Discord.User ? `${log.executor.username}#${log.executor.discriminator} (${log.executor.id})` : "Unknown",
             inline: false
-        },{
-            name: "Channel User Limit",
-            value: channel.userLimit === "0" ? "UNLIMITED" : channel.userLimit,
-            inline: false
-        });
-    }
-    if(![undefined,null,"",[],{}].includes(log) && log.action === "CHANNEL_DELETE") {
-        data.fields.push({
-           name: "Executor",
-           value: log.executor instanceof client.Discord.User ? `${log.executor.username}#${log.executor.discriminator} (${log.executor.id})` : "Unknown",
-           inline: false
-        },{
-            name: "Reason",
-            value: log.executor instanceof client.Discord.User && !log.executor.bot ? "Not Applicable" : [undefined,null,""].includes(log.reason) ? "None Specified" : log.reason,
-            inline: false
-        });
+            },{
+                name: "Reason",
+                value: log.executor instanceof client.Discord.User && !log.executor.bot ? "Not Applicable" : [undefined,null,""].includes(log.reason) ? "None Specified" : log.reason,
+                inline: false
+            });
+        }
     }
     var embed = new client.Discord.MessageEmbed(data);
     return logch.send(embed);
--- handlers/events/channelUpdate.js
+++ handlers/events/channelUpdate.js
@@ -5,21 +5,10 @@ module.exports = (async(client,oldChannel,newChannel)=>{
     if(!gConfig || [undefined,null,"",{},[]].includes(gConfig.logging) || [undefined,null,"",{},[]].includes(gConfig.logging[ev]) || !gConfig.logging[ev].enabled || [undefined,null,""].includes(gConfig.logging[ev].channel)) return;
     var logch = newChannel.guild.channels.get(gConfig.logging[ev].channel);
     if(!logch) return client.db.updateGuild({logging:{[ev]:{enabled:false,channel:null}}});
-    switch(newChannel.type) {
-        case "text":
-            var typeText = ":pencil: Text";
-            break;
+    if(newChannel.deleted) return;
 
-        case "voice":
-            var typeText = ":loudspeaker: Voice";
-            break;
-
-        case "category":
-            var typeText = "Category";
-            break;
-    }
     var base = {
-        title: `${typeText} Channel Updated`,
+        title: `${client.ucwords(newChannel.type)} Channel Updated`,
         author: {
             name: newChannel.guild.name,
             icon_url: newChannel.guild.iconURL()
@@ -27,8 +16,8 @@ module.exports = (async(client,oldChannel,newChannel)=>{
         timestamp: client.getCurrentTimestamp(),
         color: client.randomColor(),
         footer: {
-			text: `Shard ${![undefined,null].includes(newChannel.guild.shard) ? `${+newChannel.guild.shard.id+1}/${client.options.shardCount}`: "1/1"} | Bot Version ${client.config.bot.version}`
-		},
+            text: `Shard ${![undefined,null].includes(newChannel.guild.shard) ? `${+newChannel.guild.shard.id+1}/${client.options.shardCount}`: "1/1"} | Bot Version ${client.config.bot.version}`
+        },
         fields: [
             {
                 name: "Channel",
@@ -37,18 +26,40 @@ module.exports = (async(client,oldChannel,newChannel)=>{
             }
         ]
     }
-    var log = (await newChannel.guild.fetchAuditLogs({limit:1,type:"CHANNEL_UPDATE"})).entries.first();
-    if(![undefined,null,"",[],{}].includes(log) && log.action === "CHANNEL_UPDATE") {
-        var log_data = [{
-        name: "Executor",
-        value: log.executor instanceof client.Discord.User ? `${log.executor.username}#${log.executor.discriminator} (${log.executor.id})` : "Unknown",
-        inline: false
-        },{
-            name: "Reason",
-            value: log.executor instanceof client.Discord.User && !log.executor.bot ? "Not Applicable" : [undefined,null,""].includes(log.reason) ? "None Specified" : log.reason,
+
+    // audit log check
+    if(newChannel.guild.me.permissions.has("VIEW_AUDIT_LOG")) {
+        var log = (await newChannel.guild.fetchAuditLogs({limit:1,type:"CHANNEL_UPDATE"})).entries.first();
+        if(![undefined,null,"",[],{}].includes(log) && log.action === "CHANNEL_UPDATE") {
+            var log_data = [{
+            name: "Executor",
+            value: log.executor instanceof client.Discord.User ? `${log.executor.username}#${log.executor.discriminator} (${log.executor.id})` : "Unknown",
             inline: false
-        }];
+            },{
+                name: "Reason",
+                value: log.executor instanceof client.Discord.User && !log.executor.bot ? "Not Applicable" : [undefined,null,""].includes(log.reason) ? "None Specified" : log.reason,
+                inline: false
+            }];
+        }
     }
+
+    // parent
+    if(oldChannel.parent !== newChannel.parent) {
+        var data = Object.assign({},base);
+        data.fields = [{
+            name: "Old Parent Channel",
+            value: [undefined,null,""].includes(oldChannel.parent) ? "None" : `${oldChannel.parent.name} (${oldChannel.parent.id})`,
+            inline: false
+        },{
+            name: "New Parent Channel",
+            value: [undefined,null,""].includes(newChannel.parent) ? "None" : `${newChannel.parent.name} (${newChannel.parent.id})`,
+            inline: false
+        }].concat(log_data);
+        var embed = new client.Discord.MessageEmbed(data);
+        logch.send(embed);
+    }
+
+    // permission overwrites
     if(!client._.isEqual(oldChannel.permissionOverwrites.map(j=>({allow:j.allow,deny:j.deny})),newChannel.permissionOverwrites.map(j=>({allow:j.allow,deny:j.deny})))) {
         var data = Object.assign({},base);
         data.fields = [{
@@ -59,13 +70,11 @@ module.exports = (async(client,oldChannel,newChannel)=>{
         var embed = new client.Discord.MessageEmbed(data);
         logch.send(embed);
     }
+
+    // name
     if(oldChannel.name !== newChannel.name) {
         var data = Object.assign({},base);
         data.fields = [{
-            name: "Channel",
-            value: `${newChannel.name} (${newChannel.id})`,
-            inline: false
-        },{
             name: "Old Name",
             value: oldChannel.name,
             inline: false
@@ -81,11 +90,56 @@ module.exports = (async(client,oldChannel,newChannel)=>{
     switch(newChannel.type) {
         case "text":
         // topic
-        // slowmode
+        if(oldChannel.topic !== newChannel.topic && !([undefined,null,""].includes(oldChannel.topic) && [undefined,null,""].includes(newChannel.topic))) {
+            var data = Object.assign({},base);
+            data.fields = [{
+                name: "Old Topic",
+                value: [undefined,null,""].includes(oldChannel.topic) ? "None" : oldChannel.topic,
+                inline: false
+            },{
+                name: "New Topic",
+                value: [undefined,null,""].includes(newChannel.topic) ? "None" : newChannel.topic,
+                inline: false
+            }].concat(log_data);
+            var embed = new client.Discord.MessageEmbed(data);
+            logch.send(embed);
+        }
+
         // nsfw
+        if(oldChannel.nsfw !== newChannel.nsfw) {
+            var data = Object.assign({},base);
+            data.fields = [{
+                name: "Old NSFW Value",
+                value: oldChannel.nsfw ? "Yes" : "No",
+                inline: false
+            },{
+                name: "New NSFW Value",
+                value: newChannel.nsfw ? "Yes" : "No",
+                inline: false
+            }].concat(log_data);
+            var embed = new client.Discord.MessageEmbed(data);
+            logch.send(embed);
+        }
+
+        // slowmode
+        if(oldChannel.rateLimitPerUser !== newChannel.rateLimitPerUser) {
+            var data = Object.assign({},base);
+            data.fields = [{
+                name: "Old Slowmode",
+                value: oldChannel.rateLimitPerUser === 0 ? "None" : `${oldChannel.rateLimitPerUser} Seconds`,
+                inline: false
+            },{
+                name: "New Slowmode",
+                value: newChannel.rateLimitPerUser === 0 ? "None" : `${newChannel.rateLimitPerUser} Seconds`,
+                inline: false
+            }].concat(log_data);
+            var embed = new client.Discord.MessageEmbed(data);
+            logch.send(embed);
+        }
         break;
 
     case "voice":
+        // bitrate
         if(oldChannel.bitrate !== newChannel.bitrate) {
             var data = Object.assign({},base);
             data.fields = [{
@@ -101,15 +155,16 @@ module.exports = (async(client,oldChannel,newChannel)=>{
             logch.send(embed);
         }
 
+        // user limit
         if(oldChannel.userLimit !== newChannel.userLimit) {
             var data = Object.assign({},base);
             data.fields = [{
                 name: "Old User Limit",
-                value: `${oldChannel.userLimit === "0"?"UNLIMITED":oldChannel.userLimit}`,
+                value: `${oldChannel.userLimit === 0?"UNLIMITED":oldChannel.userLimit}`,
                 inline: false
             },{
                 name: "New User Limit",
-                value: `${newChannel.userLimit === "0"?"UNLIMITED":newChannel.userLimit}`,
+                value: `${newChannel.userLimit === 0?"UNLIMITED":newChannel.userLimit}`,
                 inline: false
             }].concat(log_data);
             var embed = new client.Discord.MessageEmbed(data);
