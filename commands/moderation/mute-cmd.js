2c5de6ee6ce3469c7e31dea1cba39819c31ef215
Donovan Daniels <dwddaniels262@gmail.com>
2019-01-13 22:44:39 -0600

    fix mute silent failing (long desc)
    command silently failed when no mute role was found, or user was not muted.

--- commands/moderation/mute-cmd.js
+++ commands/moderation/mute-cmd.js
@@ -24,10 +24,6 @@ module.exports = {
         
         if(!user) return message.errorEmbed("INVALID_USER");
     
-        if(user.id === message.member.id && !message.user.isDeveloper) return message.reply("Pretty sure you don't want to do this to yourclient.");
-        if(user.roles.highest.rawPosition >= message.member.roles.highest.rawPosition && message.author.id !== message.guild.owner.id) return message.reply(`You cannot mute ${user.user.tag} as their highest role is higher than yours!`);
-        if(user.permissions.has("ADMINISTRATOR")) return message.reply("That user has `ADMINISTRATOR`, that would literally do nothing.");
-        var reason = message.args.length >= 2 ? message.args.splice(1).join(" ") : "No Reason Specified";
         if(message.gConfig.muteRole === null) {
             var data = {
                 title: "No mute role",
@@ -41,7 +37,7 @@ module.exports = {
         if(!message.guild.roles.has(message.gConfig.muteRole)) {
             var data = {
                 title: "Mute role not found",
-                description: `The mute role specified for this server <@&${message.gConfig.id}> (${message.gConfig.id}) was not found, it has been reset. You can set a new one with \`${message.gConfig.prefix}setmuterole <role>\``,
+                description: `The mute role specified for this server <@&${message.gConfig.muteRole}> (${message.guild.id}) was not found, it has been reset. You can set a new one with \`${message.gConfig.prefix}setmuterole <role>\``,
                 color: 15601937
             }
             await client.db.updateGuild(message.guild.id,{muteRole:null});
@@ -52,10 +48,10 @@ module.exports = {
         if(message.guild.roles.get(message.gConfig.muteRole).rawPosition >= message.guild.me.roles.highest.rawPositon) {
             var data = {
                 title: "Invalid mute role",
-                description: `The current mute role <@&${message.gConfig.id}> (${message.gConfig.id}) seems to be higher than me, please move it below me. You can set a new one with \`${message.gConfig.prefix}setmuterole <role>\``,
+                description: `The current mute role <@&${message.gConfig.muteRole}> (${message.guild.id}) seems to be higher than me, please move it below me. You can set a new one with \`${message.gConfig.prefix}setmuterole <role>\``,
                 color: 15601937
             }
-            Object.assign(data, message.embed_defaults()("color"));
+            Object.assign(data, message.embed_defaults("color"));
             var embed = new client.Discord.MessageEmbed(data);
             return message.channel.send(embed);
         }
@@ -70,6 +66,11 @@ module.exports = {
             var embed = new client.Discord.MessageEmbed(data);
             return message.channel.send(embed);
         }
+        
+        if(user.id === message.member.id && !message.user.isDeveloper) return message.reply("Pretty sure you don't want to do this to yourclient.");
+        if(user.roles.highest.rawPosition >= message.member.roles.highest.rawPosition && message.author.id !== message.guild.owner.id) return message.reply(`You cannot mute ${user.user.tag} as their highest role is higher than yours!`);
+        if(user.permissions.has("ADMINISTRATOR")) return message.reply("That user has `ADMINISTRATOR`, that would literally do nothing.");
+        var reason = message.args.length >= 2 ? message.args.splice(1).join(" ") : "No Reason Specified";
         
         user.roles.add(message.gConfig.muteRole,`Mute: ${message.author.tag} -> ${reason}`).then(() => {
             message.channel.send(`***User ${user.user.tag} was muted, ${reason}***`).catch(noerr=>null);
