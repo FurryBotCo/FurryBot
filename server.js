755518622eaf259dafe3052dfdf327fe3d7e96cd
Donovan Daniels <dwddaniels262@gmail.com>
2019-01-15 21:22:19 -0600

    server updates

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ class FurryBotServer {
         this.https = require("https");
         this.fs = require("fs");
         this.r = require("rethinkdbdash")(this.config.db.main);
+        this.bodyParser = require("body-parser");
         //this.ro = require("rethinkdbdash")(this.config.db.other);
     }
 
@@ -27,6 +28,10 @@ class FurryBotServer {
             next();
         })
         .use(this.logger("dev"))
+        .use(this.bodyParser.json())
+        .use(this.bodyParser.urlencoded({
+            extended: true
+        }))
         .get("/stats",async(req,res)=>{
             var userCount = 0;
             var largeGuildCount=0;
@@ -148,6 +153,17 @@ class FurryBotServer {
                 shardCount: client.options.shardCount
             });
         })
+        .post("/dev/eval",checkAuth,async(req,res)=>{
+            console.log(req.body);
+            if(!req.body.code) return res.status(400).json({ success: false, message: "missing code" });
+            for(let b of  this.config.evalBlacklist) {
+                if(b.test(req.body.code)) return res.status(400).json({ success: false, message: "blacklisted code found"});
+            }
+            const start = client.performance.now(),
+            result = await eval(req.body.code),
+            end = client.performance.now();
+            return res.status(200).json({ success: true, result, time: (end-start).toFixed(3) });
+        });
         if(![undefined,null,""].includes(this.cnf.ssl) && this.cnf.ssl === true) {
             if(this.cnf.port === 80) throw new Error("ssl server cannot be ran on insecure port");
             var privateKey = this.fs.readFileSync(`${this.config.rootDir}/ssl/ssl.key`);
