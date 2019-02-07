096f40b4b9a644c68b1cdd7bd1e3465a78753ac8
Donovan Daniels <dwddaniels262@gmail.com>
2019-02-06 23:21:47 -0600

    better rate limit logging

--- handlers/events/rateLimit.js
+++ handlers/events/rateLimit.js
@@ -1,10 +1,29 @@
 module.exports = (async(client,rateLimitInfo)=>{
     client.logger.warn(`Ratelimit: ${client.util.inspect(rateLimitInfo,{showHidden: true, depth: null, color: true})}`);
-    var dt = {
-        timestamp: new Date(),
-        file: client.filename
-    };
-    
-    Object.assign(dt,rateLimitInfo);
-    client.mixpanel.track(`bot.events.rateLimit`,dt)
+    var data = {
+        title: `Ratelimited - Timeout: ${client.ms(rateLimitInfo.timeout)}`,
+        fields: [
+            {
+                name: "Limit",
+                value: rateLimitInfo.limit,
+                inline: false
+            },{
+                name: "Method",
+                value: rateLimitInfo.method.toUpperCase(),
+                inline: false
+            },{
+                name: "Path",
+                value: rateLimitInfo.path,
+                inline: false
+            },{
+                name: "Route",
+                value: rateLimitInfo.route,
+                inline: false
+            }
+        ],
+        timestamp: new Date().toISOString(),
+        color: 16762455
+    }
+    var embed = new client.Discord.MessageEmbed(data);
+    return client.channels.get(client.config.bot.channels.rateLimit).send(embed).catch(noerr => null);
 });
