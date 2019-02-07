789f2a71ce2185f310566fe5b0d190281a20a6fc
Donovan Daniels <dwddaniels262@gmail.com>
2019-02-07 10:02:44 -0600

    suggestion command is back

--- commands/misc/suggest-cmd.js
+++ commands/misc/suggest-cmd.js
@@ -12,19 +12,19 @@ module.exports = {
 	betaOnly: false,
 	guildOwnerOnly: false,
 	run: (async (client,message) => {
-		if(message.unparseArgs.length < 1 || !message.unparseArgs[0]) return new Error("ERR_INVALID_USAGE");
+		if(message.unparsedArgs.length < 1 || !message.unparsedArgs[0]) return new Error("ERR_INVALID_USAGE");
 		try {
-			var card = await client.tclient.addCard(message.unparsedArgs.join(" "),`Suggestion by ${message.author.tag} (${message.author.id}) from guild ${message.guild.name} (${message.guild.id})`,client.config.trello.list);
+			var card = await client.tclient.addCard(message.unparsedArgs.join(" "),`Suggestion by ${message.author.tag} (${message.author.id}) from guild ${message.guild.name} (${message.guild.id})`,client.config.apis.trello.list);
 		}catch(e) {
 			return message.reply(`Failed to create card: **${e.message}**`);
 		}
-		await client.tclient.addLabelToCard(card.id,client.config.trello.labels.approval).catch(error=>{return error;});
+		await client.tclient.addLabelToCard(card.id,client.config.apis.trello.labels.approval).catch(error=>{return error;});
 		//await tclient.addMemberToCard(card.id,config.trello.members.donovan_dmc).catch(error=>{return error;});
 		message.reply(`Suggestion posted!\nView it here: ${card.shortUrl}`);
 		
 		var data = {
 			title: `Suggestion by ${message.author.tag} (${message.author.id}) from guild ${message.guild.name} (${message.guild.id})`,
-			description: client.truncate(client.unparsedArgs.join(" "),950),
+			description: client.truncate(message.unparsedArgs.join(" "),950),
 			thumbnail: message.author.displayAvatarURL(),
 			fields: [
 				{
@@ -36,10 +36,10 @@ module.exports = {
 		};
 		Object.assign(data,message.embed_defaults());
 		var embed = new client.Discord.MessageEmbed(data);
-		return client.channels.get(client.config.bot.channels.suggestion).send(embed).then(message=>{
-			message.react("478050584923209728");
-			message.react("478050584898043904");
-			message.react("❌");
+		return client.channels.get(client.config.bot.channels.suggestion).send(embed).then(async(msg) => {
+			await msg.react("542963565150208001");
+			await msg.react("542963565238288384");
+			await msg.react("❌");
 		});
     })
 };
