04a4fef6b3b52ba517f753172fc897aa4495ae08
Donovan Daniels <dwddaniels262@gmail.com>
2019-04-04 00:00:29 -0500

    fix: ListStats not using correct variables

--- util/listStats.js
+++ util/listStats.js
@@ -1,13 +1,13 @@
 module.exports = (async(client) => {
 	const blapi = require("blapi"),
 		config = require("../config");
-	blapi.manualPostSharded(client.guilds.size,client.user.id,config.botLists,0,client.shards.size);
+	blapi.manualPostSharded(client.bot.guilds.size,client.bot.user.id,config.botLists,0,client.bot.shards.size);
 	// botblock was blocked on discordbots.org
-	const rq = await require("util").promisify(require("request"))(`https://discordbots.org/api/bots/${client.user.id}/stats`,{
+	const rq = await require("util").promisify(require("request"))(`https://discordbots.org/api/bots/${client.bot.user.id}/stats`,{
 		method: "POST",
 		body: JSON.stringify({
-			server_count: client.guilds.size,
-			shard_count: client.shards.size
+			server_count: client.bot.guilds.size,
+			shard_count: client.bot.shards.size
 		}),
 		headers: {
 			"Content-Type": "application/json",
@@ -15,6 +15,6 @@ module.exports = (async(client) => {
 		}
 	})
 		.then(req => JSON.parse(req.body));
-	client.logger.log(`Posted guild counts: ${client.guilds.size}`);
-	return {count:client.guilds.size};
+	client.bot.logger.log(`Posted guild counts: ${client.bot.guilds.size}`);
+	return {count:client.bot.guilds.size};
 });
