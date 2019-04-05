9ee44868f2a31c56cc548eed32777c9eb76d38ae
Donovan Daniels <dwddaniels262@gmail.com>
2019-04-05 15:18:04 -0500

    track message update

--- handlers/events/Client/messageUpdate.js
+++ handlers/events/Client/messageUpdate.js
@@ -1,4 +1,18 @@
 module.exports = (async function(message,oldMessage) {
 	if(!this || !this.mdb || message.author.bot || !message || !oldMessage || message.channel.type !== 0) return;
 	this.bot.emit("messageCreate",message);
+	if(!message.channel.guild || ![0,2,4].includes(message.channel.type)) return;
+	this.trackEvent({
+		group: "EVENTS",
+		userId: "CLIENT",
+		event: "client.events.messageUpdate",
+		properties: {
+			bot: {
+				version: this.config.bot.version,
+				beta: this.config.beta,
+				alpha: this.config.alpha,
+				server: require("os").hostname()
+			}
+		}
+	});
 });
