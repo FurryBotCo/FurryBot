64c77e44ee9baee0a06a5e26daa25b8b48fde695
Donovan Daniels <dwddaniels262@gmail.com>
2019-04-05 15:16:43 -0500

    fix: list stats posting

--- util/listStats.js
+++ util/listStats.js
@@ -1,20 +1,24 @@
 module.exports = (async(client) => {
 	const blapi = require("blapi"),
 		config = require("../config");
-	blapi.manualPostSharded(client.bot.guilds.size,client.bot.user.id,config.botLists,0,client.bot.shards.size);
-	// botblock was blocked on discordbots.org
-	const rq = await require("util").promisify(require("request"))(`https://discordbots.org/api/bots/${client.bot.user.id}/stats`,{
-		method: "POST",
-		body: JSON.stringify({
-			server_count: client.bot.guilds.size,
-			shard_count: client.bot.shards.size
-		}),
-		headers: {
-			"Content-Type": "application/json",
-			Authorization: config.botLists["discordbots.org"]
-		}
-	})
-		.then(req => JSON.parse(req.body));
-	client.bot.logger.log(`Posted guild counts: ${client.bot.guilds.size}`);
+	try {
+		blapi.manualPost(client.bot.guilds.size,client.bot.user.id,config.botLists,0,client.bot.shards.size);
+		// botblock was blocked on discordbots.org
+		const rq = await require("util").promisify(require("request"))(`https://discordbots.org/api/bots/${client.bot.user.id}/stats`,{
+			method: "POST",
+			body: JSON.stringify({
+				server_count: client.bot.guilds.size,
+				shard_count: client.bot.shards.size
+			}),
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: config.botLists["discordbots.org"]
+			}
+		})
+			.then(req => JSON.parse(req.body));
+		client.bot.logger.log(`Posted guild counts: ${client.bot.guilds.size}`);
+	} catch(e) {
+		client.logger.error(e);
+	}
 	return {count:client.bot.guilds.size};
 });
