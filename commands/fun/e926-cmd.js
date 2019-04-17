e572df88f48c0473d7063054a735818196eee031
Donovan Daniels <dwddaniels262@gmail.com>
2019-04-17 14:20:34 -0500

    fix: too many tags error on e926 command & add rating and bl test

--- commands/fun/e926-cmd.js
+++ commands/fun/e926-cmd.js
@@ -15,19 +15,23 @@ module.exports = {
 	betaOnly: false,
 	guildOwnerOnly: false,
 	run: (async function(message) {
-		let tags, bl, req, res, embed, postNumber, post;
+		let tags, bl, req, embed, postNumber, post;
 		tags = encodeURIComponent(message.args.join(" "));
 		bl = tags.match(this.config.tagBlacklist);
 		if(bl !== null && bl.length > 0) return message.channel.createMessage(`<@!${message.author.id}>, Your search contained blacklisted tags, **${bl.join("**, **")}**`);
-		req = await this.request(`https://e926.net/post/index.json?limit=50&tags=${tags}%20rating%3Asafe`,{
-			method: "GET",
-			headers: {
-				"User-Agent": this.config.web.userAgentExt("Donovan_DMC"),
-				"Content-Type": "application/json"
-			}
-		});
-		res = JSON.parse(req.body);
-		if(res.length === 0) {
+		try {
+			req = await this.phin(`https://e926.net/post/index.json?limit=50&tags=${tags}%20rating%3Asafe`,{
+				headers: {
+					"User-Agent": this.config.web.userAgentExt("Donovan_DMC"),
+					"Content-Type": "application/json"
+				}
+			}).then(res => res.body);
+		} catch(e) {
+			await message.channel.createMessage("unknown error while doing this..");
+			return this.logger.error(e);
+		}
+		if(req.success !== undefined && req.success === false) return message.channel.createMessage(`<@!${message.author.id}>, error while running command: ${req.reason}`);
+		if(req.length === 0) {
 			embed = {
 				title: "No Posts Found",
 				description: `no posts were found with the tags "${decodeURIComponent(tags)}", try another query`
@@ -35,9 +39,18 @@ module.exports = {
 			Object.assign(embed,message.embed_defaults());
 			return message.channel.createMessage({ embed });
 		} 
-		postNumber = Math.floor(Math.random(0,res.length+1) * res.length);
-		post = res[postNumber];
-		if(!post) post = res[0];
+		postNumber = Math.floor(Math.random(0,req.length+1) * req.length);
+		post = req[postNumber];
+		if(!post) post = req[0];
+		bl = post.tags.match(this.config.tagBlacklist);
+		if(![undefined,null,""].includes(bl) && bl.length === 1) {
+			this.logger.warn(`Blacklisted e926 post found, https://e926.net/post/show/${post.id}, blacklisted tag: ${bl[0]}`);
+			return message.channel.createMessage(`<@!${message.author.id}>, I couldn't return the result as it contained blacklisted a tag: **${bl[0]}**`);
+		} else if(![undefined,null,""].includes(bl) && bl.length > 1) {
+			this.logger.warn(`Blacklisted e926 post found, https://e926.net/post/show/${post.id}, blacklisted tags: ${bl.join(", ")}`);
+			return message.channel.createMessage(`<@!${message.author.id}>, I couldn't return the result as it contained blacklisted tags: **${bl.join("**, **")}**`);
+		}
+		if(!["s","safe"].includes(post.rating.toLowerCase())) return message.channel.createMessage(`<@!${message.author.id}>, API returned a non sfw image, please use the \`e621\` command if you are expecting nsfw results.`);
 		embed = {
 			title: "E926 Furs!",
 			description: `Tags: ${this.truncate(post.tags.replace("_","\\_"),1900)}\n\nLink: <https://e926.net/post/show/${post.id}>`,
