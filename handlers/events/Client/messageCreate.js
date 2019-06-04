c742f191bb7ab64c63405ed31a106a7a8a48a904
Donovan Daniels <dwddaniels262@gmail.com>
2019-06-03 22:39:38 -0500

    change requires to new version

--- handlers/events/Client/messageCreate.js
+++ handlers/events/Client/messageCreate.js
@@ -1,34 +1,15 @@
-const config = require("../../../config"),
-	Trello = require("trello"),
-	os = require("os"),
-	util = require("util"),
-	request = util.promisify(require("request")),
-	phin = require("phin").defaults({
-		method: "GET",
-		parse: "json",
-		headers: {
-			"User-Agent": config.web.userAgent
-		}
-	}),
-	uuid = require("uuid/v4"),
-	fs = require("fs"),
-	path = require("path"),
-	colors = require("console-colors-2"),
-	Canvas = require("canvas-constructor").Canvas,
-	fsn = require("fs-nextra"),
-	chalk = require("chalk"),
-	chunk = require("chunk"),
-	ytdl = require("ytdl-core"),
-	_ = require("lodash"),
-	perf = require("perf_hooks"),
-	performance = perf.performance,
-	PerformanceObserver = perf.PerformanceObserver,
-	child_process = require("child_process"),
-	shell = child_process.exec,
-	truncate = require("truncate"),
-	wordGen = require("random-words"),
-	deasync = require("deasync"),
-	{ MongoClient, mongo, mdb } = require("../../../modules/Database");
+const {
+	config,
+	os,
+	util,
+	phin,
+	performance,
+	Database: {
+		MongoClient,
+		mongo,
+		mdb
+	}
+} = require("../../../modules/CommandRequire");
 	
 module.exports = (async function (message) {
 	if(!mdb || !message) return;
