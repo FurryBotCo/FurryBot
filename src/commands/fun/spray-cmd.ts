f5726b219e373db64f6416b2259027528833ca6d
Donovan Daniels <dwddaniels262@gmail.com>
2019-07-06 14:18:31 -0500

    add spray command

---
+++ src/commands/fun/spray-cmd.ts
@@ -0,0 +1,32 @@
+import FurryBot from "@FurryBot";
+import ExtendedMessage from "@src/modules/extended/ExtendedMessage";
+import Command from "@modules/cmd/Command";
+import * as Eris from "eris";
+import functions from "@util/functions";
+import * as util from "util";
+import phin from "phin";
+import config from "@config";
+
+export default new Command({
+	triggers: [
+		"spray"
+	],
+	userPermissions: [],
+	botPermissions: [],
+	cooldown: 2e3,
+	description: "spray someone with a bottle of water..",
+	usage: "<@member/string>",
+	nsfw: false,
+	devOnly: false,
+	betaOnly: false,
+	guildOwnerOnly: false,
+	path: __filename,
+	hasSubCommands: functions.hasSubCmds(__dirname, __filename),
+	subCommands: functions.subCmds(__dirname, __filename)
+}, (async function (this: FurryBot, msg: ExtendedMessage): Promise<any> {
+	if (msg.args.length === 0) return new Error("ERR_INVALID_USAGE");
+	let input, text;
+	input = msg.args.join(" ");
+	text = functions.formatStr(msg.c, msg.author.mention, input);
+	msg.channel.createMessage(text);
+}));
