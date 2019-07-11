92ca529c5fa0bd527562d8c0d61b1fc8898e557a
Donovan Daniels <dwddaniels262@gmail.com>
2019-07-10 19:44:17 -0500

    FINALLY

--- src/api/routes/blogs.ts
+++ src/api/routes/blogs.ts
@@ -3,10 +3,14 @@ import { mdb, mongo } from "@modules/Database";
 import client from "@root/index";
 import functions from "@util/functions";
 import config from "@src/config/config";
+import util from "util";
 
 const app: express.Router = express.Router();
 
-app.get("/", async (req, res) => res.status(200).json({ success: true, blogs: await mongo.db("furrybot").collection("blogs").find({}).toArray() }))
+app.get("/", async (req, res) => res.status(200).json({
+	success: true,
+	blogs: await mongo.db("furrybot").collection("blogs").find({}).toArray().then(res => res.sort((a, b) => parseInt(a.version.replace(/\./g, ""), 10) > parseInt(b.version.replace(/\./g, ""), 10) ? -1 : parseInt(a.version.replace(/\./g, ""), 10) < parseInt(b.version.replace(/\./g, ""), 10) ? 1 : 0))
+}))
 	.get("/:id", async (req, res) => {
 		const b = await mongo.db("furrybot").collection("blogs").findOne({ version: req.params.id });
 		if (!b) return res.status(404).json({
