89c9b459544689bf57fd3aa83dd92c185f294da2
Donovan_DMC <dwddaniels262@gmail.com>
2019-08-18 18:58:29 -0500

    staff bl

---
+++ src/commands/developer/staffbl-cmd.ts
@@ -0,0 +1,30 @@
+import FurryBot from "@FurryBot";
+import ExtendedMessage from "../../modules/extended/ExtendedMessage";
+import Command from "../../modules/cmd/Command";
+import * as Eris from "eris";
+import functions from "../../util/functions";
+import * as util from "util";
+import phin from "phin";
+import config from "../../config/config";
+
+export default new Command({
+	triggers: [
+		"staffbl"
+	],
+	userPermissions: [],
+	botPermissions: [],
+	cooldown: 0,
+	description: "Blacklist someone (support server staff only)",
+	usage: "[user id]",
+	nsfw: false,
+	devOnly: true,
+	betaOnly: false,
+	guildOwnerOnly: false,
+	path: __filename,
+	hasSubCommands: functions.hasSubCmds(__dirname, __filename),
+	subCommands: functions.subCmds(__dirname, __filename)
+}, (async function (this: FurryBot, msg: ExtendedMessage): Promise<any> {
+	// extra check, to be safe
+	if (!config.developers.includes(msg.author.id)) return msg.channel.createMessage(`<@!${msg.author.id}>, You cannot run this command as you are not a developer of this bot.`);
+	return msg.channel.createMessage(msg.unparsedArgs.join(" "));
+}));
