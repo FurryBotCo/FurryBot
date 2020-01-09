7556a9860aee25ea974ebc282f89b1543ebd40eb
Donovan_DMC <dwddaniels262@gmail.com>
2020-01-08 18:34:22 -0600

    blacklist entry

--- src/util/@types/Misc.d.ts
+++ src/util/@types/Misc.d.ts
@@ -5,3 +5,26 @@ type DeepPartial<T> = {
 type ArrayOneOrMore<T> = {
 	0: T;
 } & T[];
+
+type BlacklistEntry = {
+	created: number;
+	type: "user" | "guild";
+	blame: string;
+	blameId: string;
+	reason: string;
+	id: string;
+	noticeShown: boolean;
+	expire?: number;
+	userId?: string;
+	guildId?: string;
+} & ({
+	type: "user";
+	userId: string;
+} | {
+	type: "guild";
+	guildId: string;
+});
+
+export { DeepPartial };
+export { ArrayOneOrMore };
+export { BlacklistEntry };
