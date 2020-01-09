5d865c0011144e571f640cdc58463e1eebe4da02
Donovan_DMC <dwddaniels262@gmail.com>
2020-01-08 18:31:28 -0600

    random image from lightshot command

---
+++ src/commands/nsfw/prntscr-cmd.ts
@@ -0,0 +1,91 @@
+import Command from "../../util/CommandHandler/lib/Command";
+import FurryBot from "@FurryBot";
+import ExtendedMessage from "@ExtendedMessage";
+import config from "../../config";
+import { Logger } from "../../util/LoggerV8";
+import phin from "phin";
+import * as Eris from "eris";
+import { db, mdb, mongo } from "../../modules/Database";
+import cheerio from "cheerio";
+import { Colors } from "../../util/Constants";
+
+export default new Command({
+	triggers: [
+		"prntscr",
+		"printscreen",
+		"pr"
+	],
+	userPermissions: [],
+	botPermissions: [
+		"attachFiles"
+	],
+	cooldown: 3e3,
+	donatorCooldown: 1.5e3,
+	description: "Fetch a random image from print screen (lightshot)",
+	usage: "",
+	features: ["nsfw"]
+}, (async function (this: FurryBot, msg: ExtendedMessage) {
+	const alphabet = "abcdefghijklmnopqrstuvwxyz";
+	const num = "0123456789";
+	let tries = 0;
+
+	const h = (async () => {
+		const getImage = async (): Promise<{ img: string; n: string; char: string; }> => {
+			const t = Math.random() > .5;
+			let n: string, char: string, link: string;
+
+			if (t) {
+				n = this.f.random(2, num);
+				char = this.f.random(4, alphabet);
+
+				link = `https://prnt.sc/${n}${char}`;
+			} else {
+				n = this.f.random(3, num);
+				char = this.f.random(3, alphabet);
+
+				link = `https://prnt.sc/${n}${char}`;
+			}
+
+			const p = await phin({
+				method: "GET",
+				url: link,
+				headers: {
+					"User-Agent": config.web.userAgent
+				}
+			}).catch(err => null);
+
+			const img = cheerio.load(p.body.toString())(".no-click.screenshot-image").attr("src");
+
+			const k = await phin({ method: "GET", url: img, parse: "none" }).catch(err => null);
+			if (!img || img.startsWith("//st") || !k || k.statusCode !== 200) {
+				tries++;
+				if (tries >= 5) return null;
+				return getImage();
+			} else return {
+				img,
+				n,
+				char
+			};
+		};
+
+		const i = await getImage();
+
+		if (!i) return msg.reply("failed to fetch an image after 5 tries.");
+
+		return msg.channel.createMessage({
+			embed: {
+				image: {
+					url: i.img
+				},
+				color: Colors.gold,
+				title: `${i.n}${i.char}`,
+				url: `https://prnt.sc/${i.n}${i.char}`,
+				footer: {
+					text: `random image from prnt.sc (images are not filtered)`
+				}
+			}
+		}).catch(h);
+	});
+
+	return h();
+}));
