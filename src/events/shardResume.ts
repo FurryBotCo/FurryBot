70d2c1cf9fa9c63fb2b667985ef6d2be9f3516b5
Donovan_DMC <dwddaniels262@gmail.com>
2020-01-10 14:29:15 -0600

    colors in shard events & fix shardResume

--- src/events/shardResume.ts
+++ src/events/shardResume.ts
@@ -4,16 +4,17 @@ import FurryBot from "@FurryBot";
 import * as Eris from "eris";
 import config from "../config";
 import { db } from "../modules/Database";
-import { ChannelNames, ChannelNamesCamelCase } from "../util/Constants";
+import { ChannelNames, ChannelNamesCamelCase, Colors } from "../util/Constants";
 
-export default new ClientEvent("shardDisconnect", (async function (this: FurryBot, err: Error, id: number) {
+export default new ClientEvent("shardResume", (async function (this: FurryBot, id: number) {
 	Logger.log("Shard Ready", `Shard #${id} resumed.`);
 	return this.executeWebhook(config.webhooks.shard.id, config.webhooks.shard.token, {
 		embeds: [
 			{
 				title: "Shard Resumed",
 				description: `Shard #${id} resumed.`,
-				timestamp: new Date().toISOString()
+				timestamp: new Date().toISOString(),
+				color: Colors.gold
 			}
 		],
 		username: `Furry Bot${config.beta ? " - Beta" : ""} Status`,
