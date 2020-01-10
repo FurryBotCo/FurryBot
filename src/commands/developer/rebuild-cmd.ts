326e531bc1936c92ed0bcc1768cec932623fa955
Donovan_DMC <dwddaniels262@gmail.com>
2020-01-10 14:29:34 -0600

    rebuild command

---
+++ src/commands/developer/rebuild-cmd.ts
@@ -0,0 +1,30 @@
+import Command from "../../util/CommandHandler/lib/Command";
+import FurryBot from "@FurryBot";
+import ExtendedMessage from "@ExtendedMessage";
+import config from "../../config";
+import { Logger } from "../../util/LoggerV8";
+import phin from "phin";
+import * as Eris from "eris";
+import { db, mdb, mongo } from "../../modules/Database";
+import { execSync, exec } from "child_process";
+
+export default new Command({
+	triggers: [
+		"rebuild"
+	],
+	userPermissions: [],
+	botPermissions: [],
+	cooldown: 0,
+	donatorCooldown: 0,
+	description: "Rebuild the bot's code",
+	usage: "",
+	features: ["devOnly", "betaOnly"],
+	file: __filename
+}, (async function (this: FurryBot, msg: ExtendedMessage) {
+	const start = performance.now();
+	const rb = execSync("npm run build", {
+		cwd: config.rootDir
+	});
+	const end = performance.now();
+	return msg.channel.createMessage(`Rebuild finished in ${(end - start).toFixed(3)}ms\`\`\`fix\n${rb.toString()}\n\`\`\``);
+}));
