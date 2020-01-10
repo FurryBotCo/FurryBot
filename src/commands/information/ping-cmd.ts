6d1c7c39a2a7e2498590f96d7cd591efa3169986
Donovan_DMC <dwddaniels262@gmail.com>
2020-01-10 14:28:52 -0600

    don't round ping & commas in ping

--- src/commands/information/ping-cmd.ts
+++ src/commands/information/ping-cmd.ts
@@ -25,7 +25,7 @@ export default new Command({
 	return msg.channel.createMessage("Checking Ping..")
 		.then(m => m.edit("Ping Calculated!"))
 		.then(async (m) => {
-			await msg.channel.createMessage(`Client Ping: ${+m.timestamp - +msg.timestamp}ms${"\n"}Shard Ping: ${Math.round(msg.guild.shard.latency)}ms`);
+			await msg.channel.createMessage(`Client Ping: ${Number(m.timestamp - msg.timestamp).toLocaleString()}ms${"\n"}Shard Ping: ${Number(msg.guild.shard.latency).toLocaleString()}ms`);
 			return m.delete();
 		});
 }));
