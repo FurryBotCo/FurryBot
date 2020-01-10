ac341f36ed48744fc40698469b2ebe84794a1061
Donovan_DMC <dwddaniels262@gmail.com>
2020-01-10 17:26:22 -0600

    makeinv command

---
+++ src/commands/utility/makeinv-cmd.ts
@@ -0,0 +1,75 @@
+import Command from "../../util/CommandHandler/lib/Command";
+import FurryBot from "@FurryBot";
+import ExtendedMessage from "@ExtendedMessage";
+import config from "../../config";
+import { Logger } from "../../util/LoggerV8";
+import phin from "phin";
+import * as Eris from "eris";
+import { db, mdb, mongo } from "../../modules/Database";
+import { Colors } from "../../util/Constants";
+
+export default new Command({
+	triggers: [
+		"makeinv"
+	],
+	userPermissions: [
+		"manageGuild"
+	],
+	botPermissions: [
+		"createInstantInvite"
+	],
+	cooldown: 3e3,
+	donatorCooldown: 3e3,
+	description: "Generates a unique invite to a channel.",
+	usage: "[channel] [--temporary=true/false/-t] [--maxAge=0] [--maxUses=0]",
+	features: [],
+	file: __filename
+}, (async function (this: FurryBot, msg: ExtendedMessage) {
+	if (msg.args.length === 0) return msg.channel.createMessage({
+		embed: {
+			title: "Command Help",
+			description: [
+				`As this command is a little complex in design, we have some extra help emplyed here`,
+				`Some example usages:`,
+				`Temporary invite: \`${msg.gConfig.settings.prefix}makeinv -t\``,
+				`Max Age: \`${msg.gConfig.settings.prefix}makeinv --maxAge=<seconds>\``,
+				`Max Uses: \`${msg.gConfig.settings.prefix}makeinv --maxUses=<number>\``
+			].join("\n"),
+			timestamp: new Date().toISOString(),
+			color: Colors.red
+		}
+	});
+
+	const a = msg.dashedArgs;
+	let ch: Eris.GuildChannel;
+	if (msg.args.length > 0) ch = await msg.getChannelFromArgs();
+	else ch = msg.channel;
+
+	if (!ch) ch = msg.channel;
+
+	const inv = await ch.createInvite({
+		unique: true,
+		temporary: a.value.includes("t"),
+		maxAge: Object.keys(a.keyValue).includes("maxAge") ? Number(a.keyValue.maxAge) : 0,
+		maxUses: Object.keys(a.keyValue).includes("maxUses") ? Number(a.keyValue.maxUses) : 0
+	});
+
+	return msg.channel.createMessage({
+		embed: {
+			author: {
+				name: msg.author.tag,
+				icon_url: msg.author.avatarURL
+			},
+			title: "Invite Created",
+			description: [
+				`Code: [${inv.code}](https://discord.gg/${inv.code})`,
+				`Temporary: ${inv.temporary ? "Yes" : "No"}`,
+				`Max Age: ${inv.maxAge || "None"}`,
+				`Max Uses: ${inv.maxUses || "None"}`,
+				`Channel: <#${inv.channel.id}>`
+			].join("\n"),
+			timestamp: new Date().toISOString(),
+			color: Colors.green
+		}
+	});
+}));
