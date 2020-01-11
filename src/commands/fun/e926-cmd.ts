170735c3fb2e3d10f98413d54d877e3c419d6165
Donovan_DMC <dwddaniels262@gmail.com>
2020-01-10 19:04:16 -0600

    remove reaction menus

--- src/commands/fun/e926-cmd.ts
+++ src/commands/fun/e926-cmd.ts
@@ -6,8 +6,7 @@ import { Logger } from "../../util/LoggerV8";
 import phin from "phin";
 import * as Eris from "eris";
 import { db, mdb, mongo } from "../../modules/Database";
-import ReactionQueue from "../../util/queue/ReactionQueue";
-import { ErisPermissions } from "../../util/CommandHandler/lib/utilTypes";
+import { Colors } from "../../util/Constants";
 
 export default new Command({
 	triggers: [
@@ -30,14 +29,6 @@ export default new Command({
 	if (!msg.channel.permissionsOf(this.user.id).has("manageMessages")) await msg.channel.createMessage("Warning: this command may not function properly without the `manageMessages` permission!");
 	if (this.activeReactChannels.includes(msg.channel.id) && !config.developers.includes(msg.author.id)) return msg.reply("There is already an active reaction menu in this channel. Please wait for that one to timeout, or react to the old one with \"⏹\" before starting another.");
 
-	const client = this; // tslint:disable-line no-this-assignment
-
-	const colors = {
-		green: 3066993,
-		gold: 15844367,
-		red: 15158332
-	};
-
 	const tags = msg.args.map(a => a.replace(/,\|/g, ""));
 	if (tags.length > 5) return msg.reply("you can only specify up to five (5) tags.");
 
@@ -48,33 +39,6 @@ export default new Command({
 	const e = await this.e9.listPosts(tags, 50, 1, null, config.tagBlacklist).then(res => res.filter(p => p.rating === "s"));
 
 	if (e.length === 0) return msg.reply("Your search returned no results.");
-	const perm = ["manageMessages", "addReactions"] as ErisPermissions[];
-	if (!perm.some(p => msg.channel.permissionsOf(this.user.id).has(p))) {
-		const p = perm.filter(p => !msg.channel.permissionsOf(this.user.id).has(p));
-		const embed: Eris.EmbedOptions = {
-			title: `#${e[0].id}: ${e[0].artist.join(", ").length > 256 ? "Too many artists to list." : e[0].artist.join(", ")}`,
-			url: `https://e621.net/post/show/${e[0].id}`,
-			footer: {
-				icon_url: "https://e621.net/favicon-32x32.png",
-				text: `Rating: ${e[0].rating === "s" ? "Safe" : e[0].rating === "q" ? "Questionable" : "Explicit"} | Score: ${e[0].score} - 1/${e.length}`
-			},
-			color: e[0].rating === "s" ? colors.green : e[0].rating === "q" ? colors.gold : colors.red,
-			timestamp: new Date().toISOString()
-		};
-
-		if (["jpg", "png", "gif"].includes(e[0].file_ext)) embed.image = {
-			width: e[0].width,
-			height: e[0].height,
-			url: e[0].file_url
-		};
-		else if (e[0].file_ext === "swf") embed.description = `This post is a flash animation, please directly view [the post](https://e926.net/post/show/${e[0].id}) on e926`;
-		else embed.description = `This post appears to be a video, please directly view [the post](https://e926.net/post/show/${e[0].id}) on e926`;
-
-		return msg.channel.createMessage({
-			content: `I couldn't properly display the output, as I am missing the **${p.join("**, **")}** permission(s). Please give me this permission to be able to navigate posts.`,
-			embed
-		});
-	}
 
 	let currentPost = 1;
 
@@ -85,13 +49,11 @@ export default new Command({
 			icon_url: "https://e926.net/favicon-32x32.png",
 			text: `Rating: ${e[currentPost - 1].rating === "s" ? "Safe" : e[currentPost - 1].rating === "q" ? "Questionable" : "Explicit"} | Score: ${e[currentPost - 1].score} - ${currentPost}/${e.length}`
 		},
-		color: e[currentPost - 1].rating === "s" ? colors.green : e[currentPost - 1].rating === "q" ? colors.gold : colors.red,
+		color: e[currentPost - 1].rating === "s" ? Colors.green : e[currentPost - 1].rating === "q" ? Colors.gold : Colors.red,
 		timestamp: new Date().toISOString()
 	};
 
-
 	let ratelimit = false;
-	let rt = false;
 
 	const rl = setInterval(() => ratelimit = false, 3e3);
 
@@ -109,54 +71,55 @@ export default new Command({
 	};*/
 
 	const m = await msg.channel.createMessage({ embed });
-	const q = new ReactionQueue(m);
+	const inst = await msg.channel.createMessage(`To navigate posts, you can reply with one of the following:\n**first**, **back**, **stop**, **next**, **last**.`);
 
-	const r = [
-		"⏮",
-		"◀",
-		"⏹",
-		"▶",
-		"⏭"
-	];
+	const f = (async () => {
+		const d = await this.messageCollector.awaitMessage(msg.channel.id, msg.author.id, 6e4);
+		if (!d) return setPost.call(this, "EXIT");
 
-	r.map(e => q.add({ type: "add", user: "@me", reaction: e }));
+		switch (d.content.toLowerCase()) {
+			case "first":
+				setPost.call(this, 1);
+				break;
 
-	let t = setTimeout(setPost.bind(this), 6e4, "EXIT");
-	async function setPost(this: FurryBot, p: string | number) {
-		if (!perm.some(pm => msg.channel.permissionsOf(this.user.id).has(pm))) {
-			if (rt === true) return;
-			try {
-				rt = true;
-				client.removeListener("messageReactionAdd", f);
-				await msg.reply("I seem to be missing some permissions. Please try again, and pay attention to any permission warnings!");
-			} catch (e) { }
-			return;
+			case "back":
+				setPost.call(this, currentPost - 1);
+				break;
+
+			case "stop":
+			case "exit":
+			case "cancel":
+				return setPost.call(this, "EXIT");
+				break;
+
+			case "next":
+				setPost.call(this, currentPost + 1);
+				break;
+
+			case "last":
+				setPost.call(this, e.length);
+				break;
+
+			default:
+				return;
 		}
+
+		if (msg.channel.permissionsOf(this.user.id).has("manageMessages")) await d.delete().catch(err => null);
+
+		return f();
+	});
+
+	async function setPost(this: FurryBot, p: "EXIT");
+	async function setPost(this: FurryBot, p: number);
+	async function setPost(this: FurryBot, p: string | number) {
 		if (ratelimit && !config.developers.includes(msg.author.id)) return msg.reply("You are being ratelimited! Please wait a bit more before navigating posts!").then(m => setTimeout(() => m.delete().catch(err => null), 5e3)).catch(err => null);
 		ratelimit = true;
-		clearTimeout(t);
-		t = setTimeout(setPost.bind(client), 6e4, "EXIT");
 
 		if (p === "EXIT") {
-			clearTimeout(t);
-			this.removeListener("messageReactionAdd", f);
-			if (q.entries.length > 0) {
-				let count = 0;
-				const cI = setInterval(async () => {
-					if (q.entries.length === 0 || ++count >= 20) {
-						q.destroy();
-						await m.removeReactions().catch(err => null);
-						clearInterval(cI);
-						clearInterval(rl);
-						this.activeReactChannels.splice(this.activeReactChannels.indexOf(msg.channel.id), 1);
-					}
-				});
-			} else {
-				q.destroy();
-				await m.removeReactions().catch(err => null);
-				clearInterval(rl);
-				this.activeReactChannels.splice(this.activeReactChannels.indexOf(msg.channel.id), 1);
-			}
+			clearTimeout(rl);
+			await inst.edit("Navigating not active, it either timed out or the starter exited.");
+			this.activeReactChannels.splice(this.activeReactChannels.indexOf(msg.channel.id), 1);
+			return;
 		} else currentPost = p as number;
 
 		if (currentPost === 0) currentPost = e.length;
@@ -169,7 +132,7 @@ export default new Command({
 				icon_url: "https://e926.net/favicon-32x32.png",
 				text: `Rating: ${e[currentPost - 1].rating === "s" ? "Safe" : e[currentPost - 1].rating === "q" ? "Questionable" : "Explicit"} | Score: ${e[currentPost - 1].score} - ${currentPost}/${e.length}`
 			},
-			color: e[currentPost - 1].rating === "s" ? colors.green : e[currentPost - 1].rating === "q" ? colors.gold : colors.red,
+			color: e[currentPost - 1].rating === "s" ? Colors.green : e[currentPost - 1].rating === "q" ? Colors.gold : Colors.red,
 			timestamp: new Date().toISOString()
 		};
 
@@ -179,63 +142,14 @@ export default new Command({
 			url: e[currentPost - 1].file_url
 		};
 		else if (e[currentPost - 1].file_ext === "swf") embed.description = `This post is a flash animation, please directly view [the post](https://e926.net/post/show/${e[currentPost - 1].id}) on e926`;
-		else embed.description = `This post appears to be a video, please directly view [the post](https://e926.net/post/show/${e[currentPost - 1].id}) on e926`;
+		else embed.description = `This post appears to be a video, please directly view [the post](https://e926.net/post/show/${e[currentPost - 1].id}) on e621`;
 
 		await m.edit({ embed });
-
 	}
 
-	const f = (async (ms: Eris.PossiblyUncachedMessage, emoji: Eris.Emoji, user: string) => {
-		if (!perm.some(pm => msg.channel.permissionsOf(this.user.id).has(pm))) {
-			if (rt === true) return;
-			try {
-				rt = true;
-				client.removeListener("messageReactionAdd", f);
-				await msg.reply("I seem to be missing some permissions. Please try again, and pay attention to any permission warnings!");
-			} catch (e) { }
-			return;
-		}
-		if (ms.id !== m.id || user !== msg.author.id || !r.includes(emoji.name)) {
-			if (user !== this.user.id && r.includes(emoji.name)) return q.add({
-				type: "remove",
-				reaction: emoji.id !== null ? `${emoji.name}:${emoji.id}` : emoji.name,
-				user
-			});
-			else return;
-		}
-
-		switch (emoji.name) {
-			case "⏮":
-				await setPost.call(client, 1);
-				break;
-
-			case "◀":
-				await setPost.call(client, currentPost - 1);
-				break;
-
-			case "⏹":
-				await setPost.call(client, "EXIT");
-				break;
-
-			case "▶":
-				await setPost.call(client, currentPost + 1);
-				break;
-
-			case "⏭":
-				await setPost.call(client, e.length);
-				break;
-
-			default:
-				return;
-		}
-
-		return q.add({
-			type: "remove",
-			reaction: emoji.name,
-			user
-		});
-	});
-
-	client.on("messageReactionAdd", f);
 	this.activeReactChannels.push(msg.channel.id);
+
+	f();
+
+	return;
 }));
