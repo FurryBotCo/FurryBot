991cb8b9773570bc16c3b12fd7c8d35bf2bbe17a
Donovan_DMC <dwddaniels262@gmail.com>
2020-02-03 21:19:27 -0600

    more premium stuff

--- src/commands/utility/link-cmd.ts
+++ src/commands/utility/link-cmd.ts
@@ -1,6 +1,11 @@
 import Command from "../../util/CommandHandler/lib/Command";
 import FurryBot from "@FurryBot";
 import ExtendedMessage from "@ExtendedMessage";
+import { Colors } from "../../util/Constants";
+import Eris from "eris";
+import { Internal } from "../../util/Functions";
+import config from "../../config";
+import { mdb } from "../../modules/Database";
 
 export default new Command({
 	triggers: [
@@ -15,38 +20,34 @@ export default new Command({
 	features: ["devOnly"],
 	file: __filename
 }, (async function (this: FurryBot, msg: ExtendedMessage) {
-	/*if (msg.uConfig.patreon.donator) return msg.reply("you are already marked as a donator.");
+	const c = await msg.uConfig.premiumCheck();
+	if (c.active) return msg.reply(`you are already marked as a donator.\nNote: if something has changed, and you want to refresh it, you can run \`${msg.gConfig.settings.prefix}unlink\` to remove your perks, then run this to gain them again.`);
 
-	const p = await this.f.loopPatrons();
+	const p = await Internal.loopPatrons();
 
 	for (const patron of p) {
 		const discord = patron.attributes.social_connections.discord;
 		if (discord && patron.payment_data && (discord.user_id === msg.author.id)) {
-			await msg.uConfig.edit({
-				patreon: {
-					amount: patron.payment_data.amount_cents / 100,
-					createdAt: new Date(patron.payment_data.created_at).getTime(),
-					declinedAt: new Date(patron.payment_data.declined_id).getTime(),
-					donator: true,
-					patronId: patron.id
-				}
-			}).then(d => d.reload());
+			await mdb.collection("premium").insertOne({
+				type: "user",
+				amount: patron.payment_data.amount_cents / 100,
+				activationDate: new Date(patron.payment_data.created_at).getTime(),
+				active: true,
+				patronId: patron.id,
+				userId: msg.author.id
+			} as GlobalTypes.PremiumUserEntry);
 
 			const dm = await msg.author.getDMChannel();
 
 			const embed: Eris.EmbedOptions = {
 				title: "Successfully Linked!",
-				description: `Thanks for donating! Donator perks are still in beta, so they may be a bit buggy on being enabled. You can gain some extra ${config.eco.emoji} by using \`${msg.gConfig.settings.prefix}redeem\`.`,
-				color: this.f.randomColor(),
+				description: `Thanks for donating! Donator perks are still in beta, so they may be a bit buggy on being enabled.\nIf you need any help, you can ask in our support server: ${config.bot.supportInvite}.`,
+				color: Colors.gold,
 				timestamp: new Date().toISOString()
 			};
 
 			if (!dm) await msg.channel.createMessage({ embed });
-			try {
-				await dm.createMessage({ embed });
-			} catch (e) {
-				await msg.channel.createMessage({ embed });
-			}
+			else await dm.createMessage({ embed }).catch(err => msg.channel.createMessage({ embed }));
 
 			await this.executeWebhook(config.webhooks.logs.id, config.webhooks.logs.token, {
 				embeds: [
@@ -55,16 +56,18 @@ export default new Command({
 							name: msg.author.tag,
 							icon_url: msg.author.avatarURL
 						},
-						description: `User ${msg.author.tag} (${msg.author.id}) linked their patreon account **${patron.attributes.full_name}** with the donation amount $${patron.payment_data.amount_cents / 100}, and has recieved donator perks.`
+						description: `User ${msg.author.tag} (${msg.author.id}) linked their patreon account **${patron.attributes.full_name}** with the donation amount $${patron.payment_data.amount_cents / 100}, and has recieved donator perks.`,
+						color: Colors.gold,
+						timestamp: new Date().toISOString()
 					}
 				],
 				username: `Furry Bot Donation Logs${config.beta ? " - Beta" : ""}`,
 				avatarURL: "https://i.furry.bot/furry.png"
 			});
 
-			return msg.reply(`Successfully linked your Discord account to Patreon, if you need any help, you can visit our support server here: ${config.bot.supportInvite}`);
+			return msg.reply(`successfully linked your Discord account to Patreon. If you need any help, you can visit our support server here: ${config.bot.supportInvite}`);
 		}
 
-		return msg.reply(`We were unable to link your Discord account with your Patreon account, make sure you have donated, and that the payment was successful. If you need more help, you can visit us here: ${config.bot.supportInvite}`);
-	}*/
+		return msg.reply(`we were unable to link your Discord account with your Patreon account. Make sure you have donated, and that the payment was successful. If you need more help, you can visit us here: ${config.bot.supportInvite}`);
+	}
 }));
