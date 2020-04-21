70a5ca772e5d99b9c3a1c7a362559391fe603719
Donovan_DMC <dwddaniels262@gmail.com>
2020-04-20 20:08:11 -0500

    add second user to users in conga/furpile

--- src/commands/fun/conga-cmd.ts
+++ src/commands/fun/conga-cmd.ts
@@ -17,7 +17,7 @@ export default new Command({
 	file: __filename
 }, (async function (msg, uConfig, gConfig, cmd) {
 	const h = this.holder.has("conga", msg.channel.id);
-
+	//
 	if (h && msg.args.length === 0) {
 		const c = this.holder.get<string[]>("conga", msg.channel.id);
 		const t = this.holder.get<NodeJS.Timeout>("conga", `${msg.channel.id}.timeout`);
@@ -25,7 +25,7 @@ export default new Command({
 		if (c.includes(msg.author.id) && !config.developers.includes(msg.author.id)) return msg.reply("{lang:commands.fun.conga.alreadyPresent}");
 		this.holder.add("conga", msg.channel.id, msg.author.id);
 		this.holder.set("conga", `${msg.channel.id}.timeout`, setTimeout((ch: Eris.GuildTextableChannel) => { this.holder.remove("conga", ch.id); this.holder.remove("conga", `${ch.id}.timeout`); }, 18e5, msg.channel));
-		return msg.channel.createMessage(`{lang:commands.fun.conga.join|${msg.author.id}|${c.length + 1}|${gConfig.settings.prefix}|${c.length + 1 > 30 ? Language.parseString(gConfig.settings.lang, "{lang:commands.fun.conga.tooLarge}") : `<a:${config.emojis.conga}>`.repeat(c.length + 1)}}`);
+		return msg.channel.createMessage(`{lang:commands.fun.conga.join|${msg.author.id}|${c.length + 1}|${gConfig.settings.prefix}|${c.length + 1}|${c.length + 1 > 30 ? Language.parseString(gConfig.settings.lang, "{lang:commands.fun.conga.tooLarge}") : `<a:${config.emojis.conga}>`.repeat(c.length + 1)}}`);
 	} else {
 		if (msg.args.length < 1) return new Error("ERR_INVALID_USAGE");
 		const m = await msg.getMemberFromArgs();
@@ -33,6 +33,7 @@ export default new Command({
 		if (m.id === msg.author.id) return msg.reply("{lang:commands.fun.conga.noSelf}");
 		this.holder.set("conga", msg.channel.id, []);
 		this.holder.add("conga", msg.channel.id, msg.author.id);
+		this.holder.add("conga", msg.channel.id, m.id);
 		this.holder.set("conga", `${msg.channel.id}.timeout`, setTimeout((ch: Eris.GuildTextableChannel) => { this.holder.remove("conga", ch.id); this.holder.remove("conga", `${ch.id}.timeout`); }, 18e5, msg.channel));
 		await msg.channel.createMessage(`{lang:commands.fun.conga.start|${msg.author.id}|${m.id}|${gConfig.settings.prefix}|<a:${config.emojis.conga}>}`);
 	}
