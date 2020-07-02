c2bcffccd54f20615fe7492e7b1500cf472babbe
Donovan_DMC <dwddaniels262@gmail.com>
2020-07-02 06:24:42 -0500

    other economy nonsense

---
+++ src/commands/economy/index.ts
@@ -0,0 +1,14 @@
+import Category from "../../modules/CommandHandler/Category";
+import Command from "../../modules/CommandHandler/Command";
+import * as fs from "fs-extra";
+const ext = __filename.split(".").reverse()[0];
+
+const cat = new Category({
+	name: "animals",
+	restrictions: [],
+	file: __filename
+});
+
+const cmd = fs.readdirSync(`${__dirname}`).filter(f => f.endsWith(ext) && f !== `index.${ext}` && !fs.lstatSync(`${__dirname}/${f}`).isDirectory()).map(f => require(`${__dirname}/${f}`).default);
+cmd.map(c => cat.addCommand(c.setCategory(cat.name)));
+export default cat;
