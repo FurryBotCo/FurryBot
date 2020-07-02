a5c4b3cb958c974a0e5345669b87ed6d6218dc32
Donovan_DMC <dwddaniels262@gmail.com>
2020-07-02 07:46:22 -0500

    cluster eval stuff

--- src/main.ts
+++ src/main.ts
@@ -17,12 +17,56 @@ import { Node } from "lavalink";
 import Collection from "./util/Collection";
 import MusicQueue from "./util/MusicQueue";
 import { BaseClusterWorker } from "eris-fleet";
+import { Strings } from "./util/Functions";
+import { performance } from "perf_hooks";
 
 const ew = process.emitWarning;
 process.emitWarning = ((w, name, code) => {
 	if (w.toString().indexOf("NODE_TLS_REJECT_UNAUTHORIZED") !== -1) return;
 	else return ew(w, name, code);
 });
+
+
+interface EvalMessage {
+	op: "eval";
+	msg: {
+		code: string;
+		clusterId: number;
+		resId: string;
+	};
+}
+
+interface EvalResponseMessage {
+	op: "evalResponse";
+	msg: {
+		result: string;
+		time: {
+			start: number;
+			end: number;
+			total: number;
+		};
+		clusterId: number;
+		resId: string;
+	} & ({
+		result: {
+			message: string;
+			name: string;
+			stack: string;
+		};
+		error: true;
+	} | {
+		result: string;
+		error: false;
+	});
+}
+
+class EvalError extends Error {
+	constructor(message: string, name: string, stack: string) {
+		super(message);
+		this.name = name;
+		this.stack = stack;
+	}
+}
 
 export default class FurryBot extends BaseClusterWorker {
 	holder: Holder;
@@ -35,8 +79,11 @@ export default class FurryBot extends BaseClusterWorker {
 	firstReady: boolean;
 	v: Node;
 	q: Collection<MusicQueue>;
-	threads: Map<number, Worker>;
 	bot: Eris.Client;
+	cb: Map<string, {
+		resolve: (...args: any[]) => any;
+		reject: (...args: any[]) => any;
+	}>;
 	constructor(setup) {
 		super(setup);
 		this.holder = new Holder();
@@ -65,15 +112,82 @@ export default class FurryBot extends BaseClusterWorker {
 		this.api = new API(this);
 		this.firstReady = false;
 		this.q = new Collection();
-		this.threads = new Map();
+		this.cb = new Map();
 
 		this.setup();
 	}
 
+	async evalAtCluster(id: number, code: string): Promise<{
+		time: {
+			start: number;
+			end: number;
+			total: number;
+		};
+		result: string;
+	}> {
+		return new Promise((a, b) => {
+			const resId = Strings.random(15);
+			this.ipc.sendTo(id, "eval", {
+				code,
+				clusterId: this.clusterID,
+				resId
+			} as EvalMessage["msg"]);
+
+			this.cb.set(resId, {
+				resolve: (...args) => (this.cb.delete(resId), a(...args)),
+				reject: (...args) => (this.cb.delete(resId), b(...args))
+			});
+		});
+	}
+
+	async broadcastEval(code: string): Promise<ThenArg<ReturnType<this["evalAtCluster"]>>[]> {
+		const count = Number(config.client.options.clusters) || 1;
+		const clusters = [];
+		for (let i = 0; i < count; i++) clusters.push(i);
+
+		return Promise.all(clusters.map(c => this.evalAtCluster(c, code))) as any;
+	}
+
 	// because they don't construct this until the cluster is ready??
 	async setup() {
-		this.ipc.register("eval", (...d) => console.log(d));
-		this.ipc.sendTo(0, "eval", { code: "", clusterId: this.clusterID });
+		this.ipc.register("eval", async (d: EvalMessage) => {
+			let result, error = false;
+			const start = parseFloat(performance.now().toFixed(2));
+			try {
+				result = await eval(d.msg.code);
+			} catch (e) {
+				result = {
+					message: e.message,
+					name: e.name,
+					stack: e.stack
+				};
+				error = true;
+			}
+			const end = parseFloat(performance.now().toFixed(2));
+
+			this.ipc.sendTo(d.msg.clusterId, "evalResponse", {
+				result,
+				time: {
+					start,
+					end,
+					total: parseFloat((end - start).toFixed(2))
+				},
+				error,
+				clusterId: this.clusterID,
+				resId: d.msg.resId
+			} as EvalResponseMessage["msg"]);
+		});
+		this.ipc.register("evalResponse", (d: EvalResponseMessage) => {
+			if (!this.cb.has(d.msg.resId)) throw new TypeError(`Eval Response CB "${d.msg.resId}" not found.`);
+			const { reject, resolve } = this.cb.get(d.msg.resId);
+
+			if (d.msg.error === true) reject(new EvalError(d.msg.result.message, d.msg.result.name, d.msg.result.stack));
+			else resolve({
+				time: d.msg.time,
+				result: d.msg.result
+			});
+		});
+
 		require(`${__dirname}/events/ready`).default.listener.call(this);
 
 		fs.readdirSync(`${__dirname}/events`).map(d => {
