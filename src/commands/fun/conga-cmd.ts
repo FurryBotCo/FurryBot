e75ea1f72629fc4883a4b79399d97412df415b6a
Donovan_DMC <dwddaniels262@gmail.com>
2020-07-07 06:03:06 -0500

    fix some emoji things in conga

--- src/commands/fun/conga-cmd.ts
+++ src/commands/fun/conga-cmd.ts
@@ -27,7 +27,7 @@ export default new Command({
 		if (c.includes(msg.author.id) && !config.developers.includes(msg.author.id)) return msg.reply("{lang:commands.fun.conga.alreadyPresent}");
 		this.holder.add("conga", msg.channel.id, msg.author.id);
 		this.holder.set("conga", `${msg.channel.id}.timeout`, setTimeout((ch: Eris.GuildTextableChannel) => { this.holder.remove("conga", ch.id); this.holder.remove("conga", `${ch.id}.timeout`); }, 18e5, msg.channel));
-		return msg.channel.createMessage(`{lang:commands.fun.conga.join|${msg.author.id}|${c.length + 1}|${gConfig.settings.prefix}|${c.length + 1}|${c.length + 1 > 30 ? Language.get(gConfig.settings.lang, "commands.fun.conga.tooLarge") : `<a:${config.emojis.conga}>`.repeat(c.length + 1)}}`);
+		return msg.channel.createMessage(`{lang:commands.fun.conga.join|${msg.author.id}|${c.length + 1}|${gConfig.settings.prefix}|${c.length + 1}|${c.length + 1 > 30 ? Language.get(gConfig.settings.lang, "commands.fun.conga.tooLarge") : config.emojis.conga.repeat(c.length + 1)}}`);
 	} else {
 		if (msg.args.length < 1) return new CommandError("ERR_INVALID_USAGE", cmd);
 		const m = await msg.getMemberFromArgs();
@@ -37,6 +37,6 @@ export default new Command({
 		this.holder.add("conga", msg.channel.id, msg.author.id);
 		this.holder.add("conga", msg.channel.id, m.id);
 		this.holder.set("conga", `${msg.channel.id}.timeout`, setTimeout((ch: Eris.GuildTextableChannel) => { this.holder.remove("conga", ch.id); this.holder.remove("conga", `${ch.id}.timeout`); }, 18e5, msg.channel));
-		await msg.channel.createMessage(`{lang:commands.fun.conga.start|${msg.author.id}|${m.id}|${gConfig.settings.prefix}|<a:${config.emojis.conga}>}`);
+		await msg.channel.createMessage(`{lang:commands.fun.conga.start|${msg.author.id}|${m.id}|${gConfig.settings.prefix}|${config.emojis.conga}}`);
 	}
 }));
