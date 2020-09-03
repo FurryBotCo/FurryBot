08105b925d6cb7cfa4f3f96260eb24f348c19d56
Donovan_DMC <dwddaniels262@gmail.com>
2020-09-03 00:56:47 -0500

    seen command flags

--- src/bot/commands/utility/seen-cmd.ts
+++ src/bot/commands/utility/seen-cmd.ts
@@ -12,9 +12,8 @@ export default new Command(["seen"], __filename)
 	.setCooldown(3e3, true)
 	.setExecutor(async function (msg, cmd) {
 		// @TODO across clusters
-		const showCluster = msg.dashedArgs.value.includes("cluster");
-		const showShard = msg.dashedArgs.value.includes("shard");
-		const showIds = msg.dashedArgs.value.includes("show-ids");
+		const showLocation = msg.dashedArgs.value.includes("show-location") || msg.dashedArgs.value.includes("show-all");
+		const showIds = msg.dashedArgs.value.includes("show-id") || msg.dashedArgs.value.includes("show-all");
 		let user = msg.args.length === 0 || !msg.args ? msg.author : await msg.getMemberFromArgs().then(m => !m ? null : m.user);
 		if (!user) user = await msg.getUserFromArgs();
 
@@ -22,20 +21,23 @@ export default new Command(["seen"], __filename)
 			embed: Utility.genErrorEmbed(msg.gConfig.settings.lang, "INVALID_USER", true)
 		});
 
-		const seen = this.bot.guilds.filter(g => g.members.has(user.id));
+		const seen = this.bot.guilds.filter(g => g.members.has(user.id)).sort((a, b) => a.memberCount - b.memberCount);
 		const guilds = [];
 		const fields: Eris.EmbedField[] = [];
 
 		let i = 0;
 
 		for (const s of seen) {
-			const t = `[#${s.shard.id + 1}]${showIds ? `[${s.id}]` : ""}[${s.memberCount}] ${s.name}`;
+			let v = "";
+			if (showLocation) v += `[#${this.cluster.id} / #${s.shard.id}]`; // @FIXME
+			if (showIds) v += `[${s.id}]`;
+			v += `[${s.memberCount}] ${s.name}`;
 			if (!guilds[i]) guilds[i] = "";
-			if (guilds[i].length > 1000 || +guilds[i].length + t.length > 1000) {
+			if (guilds[i].length > 1000 || +guilds[i].length + v.length > 1000) {
 				i++;
-				guilds[i] = t;
+				guilds[i] = v;
 			} else {
-				guilds[i] += `\n${t}`;
+				guilds[i] += `\n${v}`;
 			}
 		}
 
@@ -47,9 +49,14 @@ export default new Command(["seen"], __filename)
 			})
 		);
 
+		let t = "";
+		if (showLocation) t += `{lang:${cmd.lang}.info.location}`;
+		if (showIds) t += `{lang:${cmd.lang}.info.id}`;
+		t += `{lang:${cmd.lang}.info.memberCount} {lang:${cmd.lang}.info.memberCount}`;
+
 		const embed = new EmbedBuilder(msg.gConfig.settings.lang)
 			.setTitle(`{lang:${cmd.lang}.amountTitle|${seen.length}|${user.username}#${user.discriminator}|${user.id}}`)
-			.setDescription(`{lang:${cmd.lang}.amountDesc|${seen.length}}\n\n{lang:${cmd.lang}.cs${showIds ? "" : "n"}}`)
+			.setDescription(`{lang:${cmd.lang}.flags}\n\n{lang:${cmd.lang}.amountDesc|${seen.length}}\n\n${t}`)
 			.setColor(Math.random() * 0xFFFFFF)
 			.setTimestamp(new Date().toISOString());
 
