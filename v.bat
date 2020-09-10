bedb451778a5ad13c6696a1e0e3f3219150be22d
Donovan_DMC <dwddaniels262@gmail.com>
2020-09-10 09:17:10 -0500

    versioning

---
+++ v.bat
@@ -0,0 +1,10 @@
+@echo off
+npm list furrybot | grep -Po "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" > tmp.version
+set /p NPM_VERSION= < tmp.version
+del tmp.version
+FOR /f "tokens=2 delims==" %%G in ('wmic os get localdatetime /value') do set datetime=%%G & SET BUILD=%datetime:~0,4%%datetime:~4,2%%datetime:~6,2%
+
+set NEW_VERSION=%NPM_VERSION%-%BUILD%
+
+echo Adding build number %BUILD% onto the version mumber %NPM_VERSION%
+npm version %NEW_VERSION% --allow-same-version --no-git-tag-version
