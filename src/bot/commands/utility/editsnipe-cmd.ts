5211f7b23d0b55259bcd2abadca47b557ca4ec77
Donovan_DMC <dwddaniels262@gmail.com>
2020-09-20 15:07:18 -0500

    fix a small language issue in es command

--- src/bot/commands/utility/editsnipe-cmd.ts
+++ src/bot/commands/utility/editsnipe-cmd.ts
@@ -25,7 +25,7 @@ export default new Command(["editsnipe", "esnipe", "es"], __filename)
 		const author = await Redis.get(`snipe:edit:${ch.id}:author`);
 		const time = await Redis.get(`snipe:edit:${ch.id}:time`);
 
-		if (!oldContent || !newContent || !author || !time) return msg.reply(`{lang:${cmd.lang}.noSnipes|${ch.id}}`);
+		if (!oldContent || !newContent || !author || !time) return msg.reply(Language.get(msg.gConfig.settings.lang, `${cmd.lang}.noSnipes`, [ch.id]));
 		const i = newContent.match(new RegExp("((https?:\/\/)?(discord((app)?\.com\/invite|\.gg))\/[a-zA-Z0-9]{1,10})", "gi"));
 		const iN = oldContent.match(new RegExp("((https?:\/\/)?(discord((app)?\.com\/invite|\.gg))\/[a-zA-Z0-9]{1,10})", "gi"));
 		if (i) i.map(k => newContent = newContent.replace(new RegExp(k, "gi"), `[\[INVITE\]](${k})`));
