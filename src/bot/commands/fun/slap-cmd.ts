881f1dc8e4c3eb502b0ee328da5533948e352285
Donovan_DMC <dwddaniels262@gmail.com>
2020-09-29 08:57:50 -0500

    "OwO" v2

--- src/bot/commands/fun/slap-cmd.ts
+++ src/bot/commands/fun/slap-cmd.ts
@@ -19,8 +19,8 @@ export default new Command(["slap"], __filename)
 				.setAuthor(msg.author.tag, msg.author.avatarURL)
 				.setDescription(`{lang:${cmd.lang}.possible|${msg.author.id}|${Internal.extraArgParsing(msg)}}`)
 				.setTimestamp(new Date().toISOString())
-				.setFooter("OwOthis.bot.user.avatarURL)
-					.setColor(Colors.gold)
-					.toJSON()
+				.setFooter("OwO", this.bot.user.avatarURL)
+				.setColor(Colors.gold)
+				.toJSON()
 		});
 	});
