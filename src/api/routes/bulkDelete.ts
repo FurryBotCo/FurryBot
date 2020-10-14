4bb6e7d6c81c718e53d74c3a2f7aaded4c5f4da2
Donovan_DMC <dwddaniels262@gmail.com>
2020-10-13 20:36:57 -0500

    messageDeleteBulk logging

--- src/api/routes/bulkDelete.ts
+++ src/api/routes/bulkDelete.ts
@@ -16,8 +16,8 @@ export default class BulkDeleteRoute extends Route {
 			.get("/", async (req, res) => res.status(400).end("Missing Channel ID."))
 			.get("/:channelId", async (req, res) => res.status(400).end("Missing Report ID."))
 			.get("/:channelId/:reportId", async (req, res) => {
-				if (!fs.existsSync(`${config.dir.base}/src/assets/bulkDelete/${req.params.channelId}-${req.params.reportId}.txt`)) return res.status(404).end("Report not found.");
-				const report = fs.readFileSync(`${config.dir.base}/src/assets/bulkDelete/${req.params.channelId}-${req.params.reportId}.txt`).toString();
+				if (!fs.existsSync(`${config.dir.logs.bulkDelete}/${req.params.channelId}-${req.params.reportId}.txt`)) return res.status(404).end("Log not found.");
+				const report = fs.readFileSync(`${config.dir.logs.bulkDelete}/${req.params.channelId}-${req.params.reportId}.txt`).toString();
 				return res.status(200).end(report);
 			});
 	}
