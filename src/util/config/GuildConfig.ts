4d8cbfac67701cbca0cd444e640708f67fa99afe
Donovan_DMC <dwddaniels262@gmail.com>
2020-10-14 21:11:38 -0500

    remove emojiUpdate

--- src/util/config/GuildConfig.ts
+++ src/util/config/GuildConfig.ts
@@ -67,7 +67,6 @@ export default class GuildConfig {
 		"voiceSwitch" | // voiceChannelSwitch
 		"voiceStateUpdate" |
 		"guildUpdate" |
-		"emojiCreate" | // guildEmojisUpdate
 		"inviteCreate" |
 		"inviteDelete";
 	}[];
