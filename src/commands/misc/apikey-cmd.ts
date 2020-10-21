711a27e7dc61a56ab7f421e1f896f60165c22647
Donovan_DMC <dwddaniels262@gmail.com>
2020-10-21 17:03:49 -0500

    api keys maybe

---
+++ src/commands/misc/apikey-cmd.ts
@@ -0,0 +1,14 @@
+import Command from "../../util/cmd/Command";
+
+export default new Command(["apikey"], __filename)
+	.setBotPermissions([
+		"embedLinks"
+	])
+	.setUserPermissions([
+		"administrator"
+	])
+	.setRestrictions([])
+	.setCooldown(8.64e+7, true)
+	.setExecutor(async function (msg, cmd) {
+		return msg.reply("This has not been implemented yet.");
+	});
