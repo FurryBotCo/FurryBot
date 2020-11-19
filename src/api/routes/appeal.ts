831146626b9f6955095b95b14962d83f3501710c
Donovan Daniels <dwddaniels262@gmail.com>
2020-11-19 14:21:50 -0600

    make appeal endpoint send html

--- src/api/routes/appeal.ts
+++ src/api/routes/appeal.ts
@@ -12,7 +12,7 @@ export default class AppealRoute extends Route {
 		const client = this.client;
 
 		app
-			.get("/user/:id", async (req, res) => res.status(200).end(`Appeals have not been setup yet. Come ask us about it in our <a href="${config.client.socials.discord}">Support Server</a>.`))
-			.get("/guild/:id", async (req, res) => res.status(200).end(`Appeals have not been setup yet. Come ask us about it in our <a href="${config.client.socials.discord}">Support Server</a>.`));
+			.get("/user/:id", async (req, res) => res.status(200).send(`Appeals have not been setup yet. Come ask us about it in our <a href="${config.client.socials.discord}">Support Server</a>.`).end())
+			.get("/guild/:id", async (req, res) => res.status(200).send(`Appeals have not been setup yet. Come ask us about it in our <a href="${config.client.socials.discord}">Support Server</a>.`).end());
 	}
 }
