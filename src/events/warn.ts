7b76f2d5123edcec4bb973ae31d2ee5240b88019
Donovan Daniels <dwddaniels262@gmail.com>
2020-12-16 04:13:36 -0600

    ignore type 20 warnings

--- src/events/warn.ts
+++ src/events/warn.ts
@@ -2,5 +2,6 @@ import ClientEvent from "../util/ClientEvent";
 import Logger from "../util/Logger";
 
 export default new ClientEvent("warn", async function (info, id) {
+	if (info.indexOf("\"type\": 20") !== -1) return;
 	Logger.warn([`Cluster #${this.cluster.id}`, [undefined, null].includes(id) ? "General" : `Shard #${id}`], info);
 });
