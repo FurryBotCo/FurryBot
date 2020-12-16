a690a223f4b2951d8e3e90b7a39d81cad7ec77bc
Donovan Daniels <dwddaniels262@gmail.com>
2020-12-16 05:19:44 -0600

    fix this

--- src/events/warn.ts
+++ src/events/warn.ts
@@ -2,6 +2,6 @@ import ClientEvent from "../util/ClientEvent";
 import Logger from "../util/Logger";
 
 export default new ClientEvent("warn", async function (info, id) {
-	if (info.indexOf("\"type\": 20") !== -1) return;
+	if (info.indexOf("Unhandled MESSAGE_CREATE type") !== -1) return;
 	Logger.warn([`Cluster #${this.cluster.id}`, [undefined, null].includes(id) ? "General" : `Shard #${id}`], info);
 });
