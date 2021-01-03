7ef888185dab8aa9317a153ce2ab3bd780ebc361
Donovan Daniels <dwddaniels262@gmail.com>
2021-01-02 21:21:48 -0600

    good boi

--- src/commands/fun/whosagoodboi-cmd.ts
+++ src/commands/fun/whosagoodboi-cmd.ts
@@ -1,5 +1,6 @@
 import Command from "../../util/cmd/Command";
 import { Colors } from "../../util/Constants";
+import db from "../../util/Database";
 import EmbedBuilder from "../../util/EmbedBuilder";
 import Internal from "../../util/Functions/Internal";
 
@@ -12,13 +13,33 @@ export default new Command(["whosagoodboi", "whosagoodboy", "goodboi", "goodboy"
 	.setCooldown(3e3, true)
 	.setHasSlashVariant(false)
 	.setExecutor(async function (msg, cmd) {
+		const u = await msg.getUserFromArgs();
+		const naughtyList = [
+			"608483791832481813",
+			"777702082357624874"
+		];
+		if (u) {
+			const c = await db.checkBl("user", u.id);
+			if (c.current.length > 0) naughtyList.push(u.id);
+		}
 		return msg.channel.createMessage({
 			embed: new EmbedBuilder(msg.gConfig.settings.lang)
 				.setAuthor(msg.author.tag, msg.author.avatarURL)
-				.setDescription(`{lang:${cmd.lang}.${msg.args.length === 0 ? "me" : "other"}|${Internal.extraArgParsing(msg)}}`)
+				.setDescription(`{lang:${cmd.lang}.${msg.args.length === 0 ? "me" : "other"}${[u.id, msg.author.id].some(v => naughtyList.includes(v)) ? "Bad" : ""}|${Internal.extraArgParsing(msg)}}`)
 				.setTimestamp(new Date().toISOString())
 				.setFooter("OwO", this.bot.user.avatarURL)
 				.setColor(Colors.gold)
 				.toJSON()
 		});
+
+		const obj = {
+			handlers: this.cmd.handlers,
+			cool: this.cmd.cool,
+			anti: this.cmd.anti,
+			restrictions: this.cmd.restrictions,
+			categories: this.cmd.categories,
+			commands: this.cmd.commands,
+			triggers: this.cmd.triggers,
+			categoryNames: this.cmd.categoryNames
+		}
 	});
