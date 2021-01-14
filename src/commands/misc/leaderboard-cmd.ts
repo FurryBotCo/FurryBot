c9053e3b916c4be445d9dc2cb3d8813bd5d7eb1f
Donovan Daniels <dwddaniels262@gmail.com>
2021-01-14 10:58:44 -0600

    make global leaderboard look better

--- src/commands/misc/leaderboard-cmd.ts
+++ src/commands/misc/leaderboard-cmd.ts
@@ -30,47 +30,19 @@ export default new Command(["leaderboard", "lb"], __filename)
 			if (isNaN(page)) return msg.reply(Language.get(msg.gConfig.settings.lang, `${cmd.lang}.invalidPage`, [c.length]));
 			if (page < 1) return msg.reply(Language.get(msg.gConfig.settings.lang, `${cmd.lang}.pageLessThan`, [c.length]));
 			if (page > c.length) return msg.reply(Language.get(msg.gConfig.settings.lang, `${cmd.lang}.pageMoreThan`, [c.length]));
-			const content: string[] = [];
-			let b = 0;
-
-			// I would do a for-of but I need the position
-			for (let i = 0; i < c[page - 1].length; i++) {
-				const k = c[page - 1][i];
-				const l = Utility.calcLevel(k.amount);
-				const g: Eris.Guild = this.bot.guilds.get(k.guild);
-				const u: Eris.User = this.bot.users.get(k.user) || await this.getUser(k.user).catch(err => null);
-				if (!u || !g) continue;
-				if (!this.bot.users.has(u.id)) this.bot.users.set(u.id, u);
-				const pText = Internal.genTooltip(msg.gConfig.settings.lang, `**#${(i + 1) + ((page - 1) * 10)}**`, `{lang:${cmd.lang}.embed.guild|${g?.name || Language.get(msg.gConfig.settings.lang, "other.words.unknown")}|${k.guild}|${g?.memberCount || Language.get(msg.gConfig.settings.lang, "other.words.unknown")}}`);
-				const uText = Internal.genTooltip(msg.gConfig.settings.lang, `${u.username}#${u.discriminator}`, `{lang:${cmd.lang}.embed.user|${u.id}}`);
-				const text = `${pText}: ${uText} - **Level ${l.level}** (${l.leftover}/${l.leftover + l.needed} {lang:${cmd.lang}.embed.until})`;
-				if (!content[b]) {
-					content[b] = text;
-					continue;
-				}
-
-				if ((content[b].length + text.length) > 1024) {
-					b++;
-					content[b] = text;
-				} else {
-					content[b] = `${content[b]}\n${text}`;
-				}
-			}
+			const v = await Promise.all(c[page - 1].map(async ({ amount, user }, i) => {
+				const l = Utility.calcLevel(amount);
+				const u: Eris.User = await this.getUser(user).catch(err => null) || { username: "Invalid-User" };
+				return `**#${(i + 1) + ((page - 1) * 10)}**: \`${u.username.replace(/`/g, "")}\` - **Level ${l.level}** (${l.leftover}/${l.leftover + l.needed} {lang:${cmd.lang}.embed.until})`;
+			}));
 
 			const e = this.bot.users.size - entries.length;
 			return msg.channel.createMessage({
 				embed: new EmbedBuilder(msg.gConfig.settings.lang)
 					.setTitle(`{lang:${cmd.lang}.embed.titleGlobal}`)
-					.setDescription([
-						`{lang:${cmd.lang}.embed.hover}`
-					].join("\n"))
+					.setDescription(v.join("\n"))
 					.setFooter(`{lang:${cmd.lang}.embed.footer|${page}|${c.length}|${e < 0 ? 0 : e}|${time}}`)
 					.setColor(Colors.gold)
-					.addFields(...content.map((v, i) => ({
-						name: `{lang:${cmd.lang}.embed.listNum|${i + 1}}`,
-						value: v,
-						inline: false
-					})))
 					.setTimestamp(new Date().toISOString())
 					.toJSON()
 			});
