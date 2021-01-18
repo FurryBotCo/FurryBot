c70aad1340f25bdf28752889f902916b8cfa1fe5
Donovan Daniels <dwddaniels262@gmail.com>
2021-01-18 02:43:40 -0600

    fix this for the third time

--- src/commands/misc/auto-cmd.ts
+++ src/commands/misc/auto-cmd.ts
@@ -178,10 +178,10 @@ export default new Command(["auto"], __filename)
 							`{lang:other.words.example$ucwords$}: \`${msg.prefix}auto add wah 5 <#channel>\``,
 							`{lang:${cmd.lang}.help.validTimes}`,
 							`\`${msg.prefix}auto add <type> <time> <#channel>\` - {lang:${cmd.lang}.help.add}`,
-							`\`${msg.prefix}log remove <id>\` - {lang:${cmd.lang}.help.remove}`,
-							`\`${msg.prefix}log list\` - {lang:${cmd.lang}.help.list}`,
-							`\`${msg.prefix}log available\` - {lang:${cmd.lang}.help.available}`,
-							`\`${msg.prefix}log clear\` - {lang:${cmd.lang}.help.clear}`
+							`\`${msg.prefix}auto remove <id>\` - {lang:${cmd.lang}.help.remove}`,
+							`\`${msg.prefix}auto list\` - {lang:${cmd.lang}.help.list}`,
+							`\`${msg.prefix}auto available\` - {lang:${cmd.lang}.help.available}`,
+							`\`${msg.prefix}auto clear\` - {lang:${cmd.lang}.help.clear}`
 						].join("\n"))
 						.setTimestamp(new Date().toISOString())
 						.setColor(Colors.red)
