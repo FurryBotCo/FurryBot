99e1db3683a45c8d1784011d1e94c9acb6c64f71
Donovan Daniels <dwddaniels262@gmail.com>
2021-01-26 18:46:20 -0600

    make snipe like editsnipe

--- src/commands/utility/snipe-cmd.ts
+++ src/commands/utility/snipe-cmd.ts
@@ -21,9 +21,9 @@ export default new Command(["snipe"], __filename)
 		if (!ch.permissionsOf(msg.author.id).has("readMessages")) return msg.reply(Language.get(msg.gConfig.settings.lang, `${cmd.lang}.userCantSee`));
 		if (!ch.permissionsOf(this.bot.user.id).has("readMessages")) return msg.reply(Language.get(msg.gConfig.settings.lang, `${cmd.lang}.selfCantSee`));
 
-		const l = this.sn.get("delete", ch.id);
+		const [l] = this.sn.get("delete", ch.id);
 
-		if (l.length === 0) return msg.reply(Language.get(msg.gConfig.settings.lang, `${cmd.lang}.noSnipes`, [ch.id]));
+		if (!l) return msg.reply(Language.get(msg.gConfig.settings.lang, `${cmd.lang}.noSnipes`, [ch.id]));
 		const c: Eris.MessageContent[] = [
 			{
 				embed: new EmbedBuilder(msg.gConfig.settings.lang)
@@ -35,32 +35,29 @@ export default new Command(["snipe"], __filename)
 			}
 		];
 
-		for (const { content, author, time, ref } of l) {
-			const i = l.indexOf(l.find(v => v.content === content));
-			let ct = content;
-			const m = content.match(new RegExp("((https?:\/\/)?(discord((app)?\.com\/invite|\.gg))\/[a-zA-Z0-9]{1,10})", "gi"));
-			if (m !== null) m.map(k => ct = ct.replace(new RegExp(k, "gi"), `[\[INVITE\]](${k})`));
-			const a = await this.getUser(author);
-			c.push({
-				embed: new EmbedBuilder(msg.gConfig.settings.lang)
-					.setTitle(`{lang:${cmd.lang}.title|${i + 1}}`)
-					.setAuthor(`${a.username}#${a.discriminator}`, `https://cdn.discordapp.com/avatars/${a.id}/${a.avatar}.png`)
-					.setDescription([
-						`${ct.slice(0, 250)}${ct.length > 250 ? " (...)" : ""}`,
-						...(ref ? [
-							"",
-							`{lang:${cmd.lang}.reply|${ref.author}|${ref.link}}`,
-							`> ${ref.content.slice(0, 40)}${ref.content.length > 40 ? " (...)" : ""}`
-						] : [])
-					].join("\n"))
-					.setTimestamp(time)
-					.setColor(Colors.red)
-					.toJSON()
-			});
-		}
+		const m = l.content.match(new RegExp("((https?:\/\/)?(discord((app)?\.com\/invite|\.gg))\/[a-zA-Z0-9]{1,10})", "gi"));
+		if (m !== null) m.map(k => l.content = l.content.replace(new RegExp(k, "gi"), `[\[INVITE\]](${k})`));
 
 
-		this.sn.removeAll("delete", msg.channel.id);
 
-		return Promise.all(c.map(async (m) => msg.channel.createMessage(m)));
+		const u = await this.getUser(l.author);
+		this.sn.removeLast("delete", msg.channel.id);
+
+		return msg.channel.createMessage({
+			embed: new EmbedBuilder(msg.gConfig.settings.lang)
+				.setTitle(`{lang:${cmd.lang}.title}`)
+				.setAuthor(`${u.username}#${u.discriminator}`, u.avatarURL)
+				.setDescription([
+					`${l.content.slice(0, 250)}${l.content.length > 250 ? " (...)" : ""}`,
+					...(l.ref ? [
+						"",
+						`{lang:${cmd.lang}.reply|${l.ref.author}|${l.ref.link}}`,
+						`> ${l.ref.content.slice(0, 40)}${l.ref.content.length > 40 ? " (...)" : ""}`
+					] : [])
+				].join("\n"))
+				.setTimestamp(l.time)
+				.setColor(Colors.red)
+				.setFooter(`{lang:${cmd.lang}.footer}`)
+				.toJSON()
+		});
 	});
