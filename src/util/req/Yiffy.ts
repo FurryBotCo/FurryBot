903ca0dc7344e1fd457568ab812f8e2872a21b0a
Donovan Daniels <dwddaniels262@gmail.com>
2021-02-22 21:46:12 -0600

    use public url for yiffy api

--- src/util/req/Yiffy.ts
+++ src/util/req/Yiffy.ts
@@ -5,8 +5,7 @@ import Logger from "../Logger";
 const Yiffy = new YiffyAPI({
 	userAgent: config.web.userAgent,
 	apiKey: config.apis.yiffy.key,
-	debug: (url, time) => Logger.debug("Yiffy", `Request to "${url}" took ${time.time}ms`),
-	baseURL: config.beta ? "https://yiff.rest" : "https://127.2.20.1"
+	debug: (url, time) => Logger.debug("Yiffy", `Request to "${url}" took ${time.time}ms`)
 });
 
 export default Yiffy;
