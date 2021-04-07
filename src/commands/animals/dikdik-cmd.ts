71cdf184a36851ae4201754bf1cef5c36047c77c
Donovan_DMC <hewwo@yiff.rocks>
2021-04-07 17:34:05 -0500

    dikdik command

---
+++ src/commands/animals/dikdik-cmd.ts
@@ -0,0 +1,30 @@
+import Yiffy from "../../util/req/Yiffy";
+import FurryBot from "../../main";
+import UserConfig from "../../db/Models/UserConfig";
+import GuildConfig from "../../db/Models/GuildConfig";
+import { Colors, Command, EmbedBuilder } from "core";
+import Language from "language";
+
+export default new Command<FurryBot, UserConfig, GuildConfig>(["dikdik"], __filename)
+	.setBotPermissions([
+		"embedLinks",
+		"attachFiles"
+	])
+	.setUserPermissions([])
+	.setRestrictions([])
+	.setCooldown(3e3, true)
+	.setHasSlashVariant(false)
+	.setExecutor(async function (msg, cmd) {
+		const img = await Yiffy.animals.dikdik("json", 1);
+		if (!img) return msg.reply(Language.get(msg.gConfig.settings.lang, "other.errors.imageAPI"));
+		return msg.channel.createMessage({
+			embed:
+				new EmbedBuilder(msg.gConfig.settings.lang)
+					.setTitle(`{lang:${cmd.lang}.title}`)
+					.setTimestamp(new Date().toISOString())
+					.setAuthor(msg.author.tag, msg.author.avatarURL)
+					.setColor(Colors.furry)
+					.setImage(img.url)
+					.toJSON()
+		});
+	});
